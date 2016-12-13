78d89acb04053e689176f0ce542a3b6c3ef5fbbf
Eugene Skepner <github@skepner.eu>
2016-12-13 15:05:09 +0100

    pybind11 disabled warning updated

--- cc/pybind11.hh
+++ cc/pybind11.hh
@@ -4,20 +4,20 @@
 
 #pragma GCC diagnostic push
 #ifdef __clang__
-#pragma GCC diagnostic ignored "-Wreserved-id-macro" // in Python.h
-#pragma GCC diagnostic ignored "-Wextra-semi"
-#pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wmissing-noreturn"
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wdeprecated"
-#pragma GCC diagnostic ignored "-Wfloat-equal"
-#pragma GCC diagnostic ignored "-Wrange-loop-analysis"
-#pragma GCC diagnostic ignored "-Wexit-time-destructors"
-#pragma GCC diagnostic ignored "-Wundef"
-#pragma GCC diagnostic ignored "-Wdocumentation"
-#pragma GCC diagnostic ignored "-Wcovered-switch-default"
 #pragma GCC diagnostic ignored "-Wclass-varargs"
+#pragma GCC diagnostic ignored "-Wcovered-switch-default"
+#pragma GCC diagnostic ignored "-Wdeprecated"
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wexit-time-destructors"
+// #pragma GCC diagnostic ignored "-Wextra-semi"
+// #pragma GCC diagnostic ignored "-Wfloat-equal"
+// #pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wrange-loop-analysis"
+#pragma GCC diagnostic ignored "-Wreserved-id-macro" // in Python.h
+#pragma GCC diagnostic ignored "-Wshadow"
+// #pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wundef"
 #endif
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
