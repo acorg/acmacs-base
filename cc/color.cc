1b5d5d2266bca7b378257b5da2163396ea160cbd
Eugene Skepner <github@skepner.eu>
2016-12-15 18:23:46 +0100

    distinct colors fixed

--- cc/color.cc
+++ cc/color.cc
@@ -62,6 +62,34 @@ void Color::from_string(std::string aColor)
 
 // ----------------------------------------------------------------------
 
+  // http://stackoverflow.com/questions/470690/how-to-automatically-generate-n-distinct-colors (search for kellysMaxContrastSet)
+const Color::value_type Color::DistinctColors[] = {
+    0xA6BDD7, //Very Light Blue
+    0xC10020, //Vivid Red
+    0xFFB300, //Vivid Yellow
+    0x803E75, //Strong Purple
+    0xFF6800, //Vivid Orange
+    0xCEA262, //Grayish Yellow
+      //0x817066, //Medium Gray
+
+      //The following will not be good for people with defective color vision
+    0x007D34, //Vivid Green
+    0xF6768E, //Strong Purplish Pink
+    0x00538A, //Strong Blue
+    0xFF7A5C, //Strong Yellowish Pink
+    0x53377A, //Strong Violet
+    0xFF8E00, //Vivid Orange Yellow
+    0xB32851, //Strong Purplish Red
+    0xF4C800, //Vivid Greenish Yellow
+    0x7F180D, //Strong Reddish Brown
+    0x93AA00, //Vivid Yellowish Green
+    0x593315, //Deep Yellowish Brown
+    0xF13A13, //Vivid Reddish Orange
+    0x232C16, //Dark Olive Green
+};
+
+// ----------------------------------------------------------------------
+
 namespace _internal
 {
 #pragma GCC diagnostic push
