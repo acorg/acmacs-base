4935747b1330e88b46358c3306d4f1f453f6f63c
Eugene Skepner <github@skepner.eu>
2017-01-30 08:29:56 +0100

    clang temporary disabling warnings helpers

---
+++ cc/diagnostics-pop.hh
@@ -0,0 +1,6 @@
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
---
+++ cc/global-constructors-push.hh
@@ -0,0 +1,9 @@
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wglobal-constructors"
+#endif
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
