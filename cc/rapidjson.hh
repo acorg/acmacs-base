e5ca48049094f5cb1053a888f1909f38f518da45
Eugene Skepner <github@skepner.eu>
2017-07-16 19:42:10 +0200

    rapidjson DOM support

--- cc/rapidjson.hh
+++ cc/rapidjson.hh
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <stdexcept>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 // ----------------------------------------------------------------------
 
@@ -28,6 +31,14 @@ template<> inline unsigned get(const rapidjson::Value& aValue, const char* aName
 template<> inline int get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetInt(); }
 
 template<> inline rapidjson::Value::ConstArray get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetArray(); }
+
+template<> inline std::vector<std::string> get(const rapidjson::Value& aValue, const char* aName)
+{
+    std::vector<std::string> result;
+    const auto value = get<rapidjson::Value::ConstArray>(aValue, aName);
+    std::transform(value.begin(), value.end(), std::back_inserter(result), [](const auto& elt) { return elt.GetString(); });
+    return result;
+}
 
 template <typename Value> inline Value get(const rapidjson::Value& aValue, const char* aName, const Value& aDefault)
 {
