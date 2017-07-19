9dd342ac81975bfd12c1bddcbe530cf5f452e597
Eugene Skepner <github@skepner.eu>
2017-07-19 10:34:53 +0200

    get functions moved to the json_importer namespace

--- cc/rapidjson.hh
+++ cc/rapidjson.hh
@@ -23,32 +23,57 @@ class RapidjsonAssert : public std::exception { public: using std::exception::ex
 
 // ----------------------------------------------------------------------
 
-template <typename Value> Value get(const rapidjson::Value& aValue, const char* aName);
-
-template<> inline std::string get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetString(); }
-template<> inline size_t get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
-template<> inline unsigned get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
-template<> inline int get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetInt(); }
-
-template<> inline rapidjson::Value::ConstArray get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetArray(); }
-
-template<> inline std::vector<std::string> get(const rapidjson::Value& aValue, const char* aName)
+namespace json_importer
 {
-    std::vector<std::string> result;
-    const auto value = get<rapidjson::Value::ConstArray>(aValue, aName);
-    std::transform(value.begin(), value.end(), std::back_inserter(result), [](const auto& elt) { return elt.GetString(); });
-    return result;
-}
+    template <typename Value> Value get(const rapidjson::Value& aValue, const char* aName);
 
-template <typename Value> inline Value get(const rapidjson::Value& aValue, const char* aName, const Value& aDefault)
-{
-    try {
-        return get<Value>(aValue, aName);
+    template<> inline std::string get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetString(); }
+    template<> inline size_t get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
+    template<> inline unsigned get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
+    template<> inline int get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetInt(); }
+
+    template<> inline rapidjson::Value::ConstArray get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetArray(); }
+
+    template<> inline std::vector<std::string> get(const rapidjson::Value& aValue, const char* aName)
+    {
+        std::vector<std::string> result;
+        const auto value = get<rapidjson::Value::ConstArray>(aValue, aName);
+        std::transform(value.begin(), value.end(), std::back_inserter(result), [](const auto& elt) { return elt.GetString(); });
+        return result;
     }
-    catch (RapidjsonAssert&) {
-        return aDefault;
+
+    template <typename Value> inline Value get(const rapidjson::Value& aValue, const char* aName, const Value& aDefault)
+    {
+        try {
+            return get<Value>(aValue, aName);
+        }
+        catch (RapidjsonAssert&) {
+            return aDefault;
+        }
     }
-}
+
+// ----------------------------------------------------------------------
+
+    class Object
+    {
+     public:
+        inline Object(std::string aSrc) { mDoc.Parse(aSrc.c_str(), aSrc.size()); }
+        inline Object(Object&& aSrc) : mDoc{std::move(aSrc.mDoc)} {}
+        virtual inline ~Object() {}
+
+        template <typename Value> inline Value get(const char* aName) const { return json_importer::get<Value>(mDoc, aName); }
+        template <typename Value> inline Value get(const char* aName, const Value& aDefault) const { return json_importer::get(mDoc, aName, aDefault); }
+
+        inline std::string get_string(const char* aName) const { return get(aName, std::string{}); }
+
+     private:
+        rapidjson::Document mDoc;
+
+    }; // class Object
+
+// ----------------------------------------------------------------------
+
+} // namespace json_importer
 
 // ----------------------------------------------------------------------
 /// Local Variables:
