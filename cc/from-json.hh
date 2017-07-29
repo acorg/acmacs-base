8845c7271202106a3294a50e680067c7e6afc5c0
Eugene Skepner <github@skepner.eu>
2017-07-29 16:54:58 +0200

    to-json and from-json modules (for rapidjson DOM interface)

---
+++ cc/from-json.hh
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "acmacs-base/string.hh"
+
+// ----------------------------------------------------------------------
+
+namespace from_json
+{
+    class rapidjson_assert : public std::exception { public: using std::exception::exception; };
+}
+
+#ifdef RAPIDJSON_ASSERT
+#undef RAPIDJSON_ASSERT
+#endif
+#define RAPIDJSON_ASSERT(x) {if (!(x)) throw from_json::rapidjson_assert{}; }
+
+// ----------------------------------------------------------------------
+
+#pragma GCC diagnostic push
+#include "acmacs-base/rapidjson-diagnostics.hh"
+#include "rapidjson/reader.h"
+#include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
+#include "rapidjson/istreamwrapper.h"
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+
+namespace from_json
+{
+    using ConstArray = rapidjson::Value::ConstArray;
+
+    template <typename Value> Value get(const rapidjson::Value& aValue, const char* aName);
+
+    template<> inline std::string get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetString(); }
+    template<> inline size_t get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
+    template<> inline unsigned get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetUint(); }
+    template<> inline int get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetInt(); }
+    template<> inline bool get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetBool(); }
+
+    template<> inline ConstArray get(const rapidjson::Value& aValue, const char* aName) { return aValue[aName].GetArray(); }
+
+    inline const char* get_string(const rapidjson::Value& aValue) { return aValue.GetString(); }
+    inline std::string get_string_uppercase(const rapidjson::Value& aValue) { return string::upper(aValue.GetString()); }
+    inline std::string get_string_lowercase(const rapidjson::Value& aValue) { return string::lower(aValue.GetString()); }
+
+    template<> inline std::vector<std::string> get(const rapidjson::Value& aValue, const char* aName)
+    {
+        std::vector<std::string> result;
+        const auto value = get<ConstArray>(aValue, aName);
+          //std::transform(value.begin(), value.end(), std::back_inserter(result), [](const auto& elt) { return elt.GetString(); });
+        std::transform(value.begin(), value.end(), std::back_inserter(result), &get_string);
+        return result;
+    }
+
+    template <typename Value> inline Value get(const rapidjson::Value& aValue, const char* aName, const Value& aDefault)
+    {
+        try {
+            return get<Value>(aValue, aName);
+        }
+        catch (rapidjson_assert&) {
+            return aDefault;
+        }
+    }
+
+// ----------------------------------------------------------------------
+
+    class object
+    {
+     public:
+        inline object(std::string aSrc) { mDoc.Parse(aSrc.c_str(), aSrc.size()); }
+        inline object(object&& aSrc) : mDoc{std::move(aSrc.mDoc)} {}
+        inline object(std::istream&& aInput) { if (!aInput) throw std::runtime_error{"cannot read json input"}; rapidjson::IStreamWrapper wrapper{aInput}; mDoc.ParseStream(wrapper); }
+        virtual inline ~object() {}
+
+        template <typename Value> inline Value get(const char* aName) const { return from_json::get<Value>(mDoc, aName); }
+        template <typename Value> inline Value get(const char* aName, const Value& aDefault) const { return from_json::get(mDoc, aName, aDefault); }
+
+        inline std::string get_string(const char* aName) const { return get(aName, std::string{}); }
+        inline ConstArray get_array(const char* aName) const { return from_json::get<ConstArray>(mDoc, aName); } // throws rapidjson_assert
+
+     private:
+        rapidjson::Document mDoc;
+
+    }; // class Object
+
+} // namespace from_json
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
---
+++ cc/to-json.hh
@@ -0,0 +1,121 @@
+#pragma once
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// ----------------------------------------------------------------------
+
+#pragma GCC diagnostic push
+#include "acmacs-base/rapidjson-diagnostics.hh"
+#include "rapidjson/reader.h"
+#include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
+#include "rapidjson/istreamwrapper.h"
+#pragma GCC diagnostic pop
+
+#include "acmacs-base/float.hh"
+
+// ----------------------------------------------------------------------
+
+namespace to_json
+{
+    class raw : public std::string
+    {
+     public:
+        inline raw(const std::string& src) : std::string{src} {}
+        inline raw(std::string&& src) : std::string{std::move(src)} {}
+    };
+
+// ----------------------------------------------------------------------
+
+    namespace internal
+    {
+        inline std::string make_value(std::string value) { return "\"" + value + "\""; }
+        inline std::string make_value(to_json::raw value) { return value; }
+        inline std::string make_value(const char* value) { return std::string{"\""} + value + "\""; }
+        inline std::string make_value(bool value) { return value ? "true" : "false"; }
+        inline std::string make_value(std::nullptr_t) { return "null"; }
+        template <typename Value> inline typename std::enable_if<std::numeric_limits<Value>::is_integer, std::string>::type make_value(Value value) { return std::to_string(value); }
+        template <typename Value> inline typename std::enable_if<std::is_floating_point<Value>::value, std::string>::type make_value(Value value) { return double_to_string(value); }
+
+        template <typename Value> inline std::string object_append(std::string target, std::string key, Value value)
+        {
+            std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"};
+            result += "\"" + key + "\":" + make_value(value) + "}";
+            return result;
+        }
+
+        template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value value, Args ... args)
+        {
+            return object_append(object_append(target, key, value), args ...);
+        }
+
+        inline std::string join(std::string left, std::string right)
+        {
+            if (left.size() < 3)
+                return right;
+            else if (right.size() < 3)
+                return left;
+            else
+                return left.substr(0, left.size() - 1) + "," + right.substr(1);
+        }
+
+        template <typename Value> inline std::string array_append(std::string target, Value value)
+        {
+            std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["};
+            result += internal::make_value(value) + "]";
+            return result;
+        }
+
+        template <typename Value, typename ... Args> inline std::string array_append(std::string target, Value value, Args ... args)
+        {
+            return array_append(array_append(target, value), args ...);
+        }
+
+    } // namespace internal
+
+// ----------------------------------------------------------------------
+
+
+    template <typename Value> inline std::string value(Value value)
+    {
+        return internal::make_value(value);
+    }
+
+    template <typename ... Args> inline std::string object(Args ... args)
+    {
+        return internal::object_append(std::string{}, args ...);
+    }
+
+    template <typename ... Args> inline std::string object_prepend(std::string target, Args ... args)
+    {
+        return internal::join(object(args ...), target);
+    }
+
+// ----------------------------------------------------------------------
+
+    template <typename ... Args> inline std::string array(Args ... args)
+    {
+        return internal::array_append(std::string{}, args ...);
+    }
+
+    template <typename ... Args> inline std::string array_append(std::string target, Args ... args)
+    {
+        return internal::join(target, array(args ...));
+    }
+
+    template <typename ... Args> inline std::string array_prepend(std::string target, Args ... args)
+    {
+        return internal::join(array(args ...), target);
+    }
+
+// ----------------------------------------------------------------------
+
+} // namespace to_json
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
