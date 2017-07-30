06c201636b9505f79f5ed754b40a8b4b196e386f
Eugene Skepner <github@skepner.eu>
2017-07-30 10:27:30 +0200

    using if constexpr

--- cc/to-json.hh
+++ cc/to-json.hh
@@ -25,32 +25,29 @@ namespace to_json
 
     namespace internal
     {
-        inline std::string make_value(std::string value) { return "\"" + value + "\""; }
-        inline std::string make_value(to_json::raw value) { return value; }
-        inline std::string make_value(const char* value) { return std::string{"\""} + value + "\""; }
-        inline std::string make_value(bool value) { return value ? "true" : "false"; }
-        inline std::string make_value(std::nullptr_t) { return "null"; }
-        template <typename Value> inline typename std::enable_if<std::numeric_limits<Value>::is_integer, std::string>::type make_value(Value value) { return std::to_string(value); }
-        template <typename Value> inline typename std::enable_if<std::is_floating_point<Value>::value, std::string>::type make_value(Value value) { return double_to_string(value); }
+        template <typename T> inline std::string make_value(T value)
+        {
+            if constexpr      (std::is_same<std::string, T>::value)    { return "\"" + value + "\""; }
+            else if constexpr (std::is_same<to_json::raw, T>::value)   { return value; }
+            else if constexpr (std::is_same<const char*, T>::value)    { return std::string{"\""} + value + "\""; }
+            else if constexpr (std::is_same<bool, T>::value)           { return value ? "true" : "false"; }
+            else if constexpr (std::is_same<std::nullptr_t, T>::value) { return "null"; }
+            else if constexpr (std::numeric_limits<T>::is_integer)     { return std::to_string(value); }
+            else if constexpr (std::is_floating_point<T>::value)       { return double_to_string(value); }
+        }
 
-        // template <typename Value> inline std::string object_append(std::string target, std::string key, Value value)
-        // {
-        //     std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"};
-        //     result += "\"" + key + "\":" + make_value(value) + "}";
-        //     return result;
-        // }
-
-        // template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value value, Args ... args)
-        // {
-        //     return object_append(object_append(target, key, value), args ...);
-        // }
+        // inline std::string make_value(std::string value) { return "\"" + value + "\""; }
+        // inline std::string make_value(to_json::raw value) { return value; }
+        // inline std::string make_value(const char* value) { return std::string{"\""} + value + "\""; }
+        // inline std::string make_value(bool value) { return value ? "true" : "false"; }
+        // inline std::string make_value(std::nullptr_t) { return "null"; }
+        // template <typename Value> inline typename std::enable_if<std::numeric_limits<Value>::is_integer, std::string>::type make_value(Value value) { return std::to_string(value); }
+        // template <typename Value> inline typename std::enable_if<std::is_floating_point<Value>::value, std::string>::type make_value(Value value) { return double_to_string(value); }
 
         template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value value, Args ... args)
         {
             if constexpr (sizeof...(args) == 0) {
-                    std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"};
-                    result += "\"" + key + "\":" + make_value(value) + "}";
-                    return result;
+                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"}) + "\"" + key + "\":" + make_value(value) + "}";
                 }
             else {
                 return object_append(object_append(target, key, value), args ...);
@@ -67,17 +64,39 @@ namespace to_json
                 return left.substr(0, left.size() - 1) + "," + right.substr(1);
         }
 
-        template <typename Value> inline std::string array_append(std::string target, Value value)
-        {
-            std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["};
-            result += internal::make_value(value) + "]";
-            return result;
-        }
-
         template <typename Value, typename ... Args> inline std::string array_append(std::string target, Value value, Args ... args)
         {
-            return array_append(array_append(target, value), args ...);
+            if constexpr (sizeof...(args) == 0) {
+                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["}) + internal::make_value(value) + "]";
+                }
+            else {
+                return array_append(array_append(target, value), args ...);
+            }
         }
+
+        // template <typename Value> inline std::string object_append(std::string target, std::string key, Value value)
+        // {
+        //     std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"};
+        //     result += "\"" + key + "\":" + make_value(value) + "}";
+        //     return result;
+        // }
+
+        // template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value value, Args ... args)
+        // {
+        //     return object_append(object_append(target, key, value), args ...);
+        // }
+
+        // template <typename Value> inline std::string array_append(std::string target, Value value)
+        // {
+        //     std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["};
+        //     result += internal::make_value(value) + "]";
+        //     return result;
+        // }
+
+        // template <typename Value, typename ... Args> inline std::string array_append(std::string target, Value value, Args ... args)
+        // {
+        //     return array_append(array_append(target, value), args ...);
+        // }
 
     } // namespace internal
 
