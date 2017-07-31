c4f8d92094739d96bdffe734a18e616fff30828c
Eugene Skepner <github@skepner.eu>
2017-07-31 12:00:35 +0200

    to_json improvements

--- cc/to-json.hh
+++ cc/to-json.hh
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <typeinfo>
 
 // ----------------------------------------------------------------------
 
@@ -23,19 +24,17 @@ namespace to_json
 
 // ----------------------------------------------------------------------
 
-    namespace internal
+    template <typename T> inline std::string value(T aValue)
     {
-        template <typename T> inline std::string make_value(T value)
-        {
-            if constexpr      (std::is_same<std::string, T>::value)    { return "\"" + value + "\""; }
-            else if constexpr (std::is_same<to_json::raw, T>::value)   { return value; }
-            else if constexpr (std::is_same<const char*, T>::value)    { return std::string{"\""} + value + "\""; }
-            else if constexpr (std::is_same<bool, T>::value)           { return value ? "true" : "false"; }
-            else if constexpr (std::is_same<std::nullptr_t, T>::value) { return "null"; }
-            else if constexpr (std::numeric_limits<T>::is_integer)     { return std::to_string(value); }
-            else if constexpr (std::is_floating_point<T>::value)       { return double_to_string(value); }
-            else                                                       { return std::to_string(value); }
-        }
+        if constexpr      (std::is_same<std::string, T>::value)    { return "\"" + aValue + "\""; }
+        else if constexpr (std::is_same<to_json::raw, T>::value)   { return aValue; }
+        else if constexpr (std::is_same<const char*, T>::value)    { return std::string{"\""} + aValue + "\""; }
+        else if constexpr (std::is_same<bool, T>::value)           { return aValue ? "true" : "false"; }
+        else if constexpr (std::is_same<std::nullptr_t, T>::value) { return "null"; }
+        else if constexpr (std::numeric_limits<T>::is_integer)     { return std::to_string(aValue); }
+        else if constexpr (std::is_floating_point<T>::value)       { return double_to_string(aValue); }
+        else                                                       { throw std::runtime_error{std::string{"to_json::make_value called for "} + typeid(aValue).name()}; }
+    }
 
         // inline std::string make_value(std::string value) { return "\"" + value + "\""; }
         // inline std::string make_value(to_json::raw value) { return value; }
@@ -45,13 +44,17 @@ namespace to_json
         // template <typename Value> inline typename std::enable_if<std::numeric_limits<Value>::is_integer, std::string>::type make_value(Value value) { return std::to_string(value); }
         // template <typename Value> inline typename std::enable_if<std::is_floating_point<Value>::value, std::string>::type make_value(Value value) { return double_to_string(value); }
 
-        template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value value, Args ... args)
+// ----------------------------------------------------------------------
+
+    namespace internal
+    {
+        template <typename Value, typename ... Args> inline std::string object_append(std::string target, std::string key, Value aValue, Args ... args)
         {
             if constexpr (sizeof...(args) == 0) {
-                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"}) + "\"" + key + "\":" + make_value(value) + "}";
+                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"}) + "\"" + key + "\":" + value(aValue) + "}";
                 }
             else {
-                return object_append(object_append(target, key, value), args ...);
+                return object_append(object_append(target, key, aValue), args ...);
             }
         }
 
@@ -65,20 +68,20 @@ namespace to_json
                 return left.substr(0, left.size() - 1) + "," + right.substr(1);
         }
 
-        template <typename Value, typename ... Args> inline std::string array_append(std::string target, Value value, Args ... args)
+        template <typename Value, typename ... Args> inline std::string array_append(std::string target, Value aValue, Args ... args)
         {
             if constexpr (sizeof...(args) == 0) {
-                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["}) + internal::make_value(value) + "]";
+                    return (target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["}) + value(aValue) + "]";
                 }
             else {
-                return array_append(array_append(target, value), args ...);
+                return array_append(array_append(target, aValue), args ...);
             }
         }
 
         // template <typename Value> inline std::string object_append(std::string target, std::string key, Value value)
         // {
         //     std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"{"};
-        //     result += "\"" + key + "\":" + make_value(value) + "}";
+        //     result += "\"" + key + "\":" + value(value) + "}";
         //     return result;
         // }
 
@@ -90,7 +93,7 @@ namespace to_json
         // template <typename Value> inline std::string array_append(std::string target, Value value)
         // {
         //     std::string result = target.size() > 2 ? target.substr(0, target.size() - 1) + "," : std::string{"["};
-        //     result += internal::make_value(value) + "]";
+        //     result += internal::value(value) + "]";
         //     return result;
         // }
 
@@ -103,11 +106,6 @@ namespace to_json
 
 // ----------------------------------------------------------------------
 
-
-    template <typename Value> inline std::string value(Value value)
-    {
-        return internal::make_value(value);
-    }
 
     template <typename ... Args> inline std::string object(Args ... args)
     {
