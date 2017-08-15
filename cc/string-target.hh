b0e26343611a4a394380cfeec4e68b9787cb212d
Eugene Skepner <github@skepner.eu>
2017-08-15 16:50:29 +0200

    support for different string manipulation routines implementations for Unix and Browser

---
+++ cc/string-target.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "acmacs-base/config.hh"
+
+#ifdef ACMACS_TARGET_OS
+#include "acmacs-base/string.hh"
+#endif
+
+#ifdef ACMACS_TARGET_BROWSER
+#include "client/string-client.hh"
+#endif
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
