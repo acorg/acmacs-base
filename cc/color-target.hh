5d1fdd6f0dc55c2d708bc52af1f12b8fba92d51c
Eugene Skepner <github@skepner.eu>
2017-08-15 16:42:30 +0200

    support for different Color implementations for Unix and Browser

---
+++ cc/color-target.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "acmacs-base/config.hh"
+
+#ifdef ACMACS_TARGET_OS
+#include "acmacs-base/color.hh"
+#endif
+
+#ifdef ACMACS_TARGET_BROWSER
+#include "client/color.hh"
+#endif
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
