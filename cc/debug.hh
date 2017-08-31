f492d1b584877c9e7c353dc5aeaef54e9957f3d9
Eugene Skepner <github@skepner.eu>
2017-08-31 13:09:12 +0200

    DEBUG_LINE_FILE macro

---
+++ cc/debug.hh
@@ -0,0 +1,10 @@
+#pragma once
+
+// ----------------------------------------------------------------------
+
+#define DEBUG_LINE_FUNC (std::string{" ["} + __FILE__ + ":" + std::to_string(__LINE__) + ": " + __PRETTY_FUNCTION__ + "]")
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
