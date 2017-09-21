ccb282c8843cf1b625fc217388aaaf57c18c3ecc
Eugene Skepner <github@skepner.eu>
2017-09-21 17:56:42 +0200

    Timeit improvements

--- cc/timeit.hh
+++ cc/timeit.hh
@@ -10,27 +10,27 @@
 class Timeit
 {
  public:
-    inline Timeit(std::string msg, std::ostream& out = std::cout)
-        : message(msg), out_stream(out), start(std::chrono::steady_clock::now()) {}
+    inline Timeit(std::string msg, std::ostream& out = std::cout, bool aReport = true)
+        : message(msg), out_stream(out), mReport(aReport), start(std::chrono::steady_clock::now()) {}
 
     inline ~Timeit() { report(); }
 
     inline void report()
         {
-            if (!reported) {
+            if (mReport) {
                 const auto total = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
                 const decltype(total) s = total / 1000;
                 const decltype(total) ms = total % 1000;
                 out_stream << message << s << '.' << std::setw(3) << std::setfill('0') << ms << std::endl;
-                reported = true;
+                mReport = false;
             }
         }
 
  private:
     std::string message;
     std::ostream& out_stream;
+    bool mReport;
     decltype(std::chrono::steady_clock::now()) start;
-    bool reported = false;
 };
 
 // ----------------------------------------------------------------------
