694b009d56057c38abdeedf9a0e09d962892fb37
Eugene Skepner <github@skepner.eu>
2017-09-28 09:16:05 +0200

    using std::void_t

--- cc/sfinae.hh
+++ cc/sfinae.hh
@@ -6,7 +6,7 @@
 // ----------------------------------------------------------------------
 
   // void_t is a C++17 feature
-template<class ...> using void_t = void; // http://stackoverflow.com/questions/26513095/void-t-can-implement-concepts
+// template<class ...> using void_t = void; // http://stackoverflow.com/questions/26513095/void-t-can-implement-concepts
 
 // ----------------------------------------------------------------------
 
@@ -16,7 +16,7 @@ namespace ad_sfinae
 // ----------------------------------------------------------------------
 
     template <typename T, typename = void> struct castable_to_char : public std::false_type {};
-    template <typename T> struct castable_to_char<T, void_t<decltype(static_cast<char>(std::declval<T>()))>> : public std::true_type {};
+    template <typename T> struct castable_to_char<T, std::void_t<decltype(static_cast<char>(std::declval<T>()))>> : public std::true_type {};
 
 // ----------------------------------------------------------------------
 
@@ -26,7 +26,7 @@ namespace ad_sfinae
     template <typename T, typename = void> struct is_std_container : std::false_type { };
 
     template <typename T> struct is_std_container<T,
-                                                  void_t<decltype(std::begin(std::declval<T&>())),
+                                                  std::void_t<decltype(std::begin(std::declval<T&>())),
                                                          decltype(std::end(std::declval<T&>())),
                                                          typename T::value_type
                                                          >>
@@ -37,7 +37,7 @@ namespace ad_sfinae
     // template <typename T, typename = void> struct is_list : std::false_type { };
 
     // template <typename T> struct is_list<T,
-    //                                      void_t<decltype(std::begin(std::declval<T&>())),
+    //                                      std::void_t<decltype(std::begin(std::declval<T&>())),
     //                                             decltype(std::end(std::declval<T&>())),
     //                                             decltype(std::declval<T&>().operator[](static_cast<size_t>(0))),
     //                                             typename T::value_type
