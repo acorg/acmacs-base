2fad73d7dae7df34f4b5b3178dfc500dbf88075d
Eugene Skepner <github@skepner.eu>
2017-10-23 18:07:22 +0200

    argc_argv: support for multiple occurences of a switch with string arg

--- cc/argc-argv.cc
+++ cc/argc-argv.cc
@@ -24,9 +24,19 @@ argc_argv::argc_argv(int argc, const char* const argv[], std::initializer_list<s
                     using T = std::decay_t<decltype(arg)>;
                     if constexpr (std::is_same_v<T, bool>)
                         mOptions.emplace_back(name, !arg);
-                    else if constexpr (std::is_same_v<T, const char*>) {
+                    else if constexpr (std::is_same_v<T, string>) {
                         if ((arg_no + 1) < argc)
                             mOptions.emplace_back(name, option_default{argv[++arg_no]});
+                        else
+                            throw option_requires_argument{name + std::string{" requires argument"}};
+                    }
+                    else if constexpr (std::is_same_v<T, strings>) {
+                        if ((arg_no + 1) < argc) {
+                            if (const auto found_opt = std::find_if(std::begin(mOptions), std::end(mOptions), [&name](const auto& opt) { return opt.first == name; }); found_opt == std::end(mOptions))
+                                mOptions.emplace_back(name, option_default{strings{argv[++arg_no]}});
+                            else
+                                std::get<T>(found_opt->second).push_back(argv[++arg_no]);
+                        }
                         else
                             throw option_requires_argument{name + std::string{" requires argument"}};
                     }
@@ -90,8 +100,10 @@ std::string argc_argv::usage_options(size_t aIndent) const
                        std::string result;
                        if constexpr (std::is_same_v<T, bool>)
                                             ;
-                       else if constexpr (std::is_same_v<T, const char*>)
+                       else if constexpr (std::is_same_v<T, string>)
                                                  result = std::string{' ', '"'} + arg + "\"";
+                       else if constexpr (std::is_same_v<T, strings>)
+                                                 result = " \"(multiple-occurneces-ot-switch-hanled)\"";
                        else if constexpr (std::is_same_v<T, long>)
                                                  result = " " + std::to_string(arg);
                        else if constexpr (std::is_same_v<T, double>)
@@ -117,198 +129,198 @@ std::string argc_argv::usage_options(size_t aIndent) const
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 
-argc_argv_simple::option::~option()
-{
-} // argc_argv_simple::option::~option
+// argc_argv_simple::option::~option()
+// {
+// } // argc_argv_simple::option::~option
 
-argc_argv_simple::option::operator bool() const
-{
-    return true;
+// argc_argv_simple::option::operator bool() const
+// {
+//     return true;
 
-} // argc_argv_simple::option::operator bool
+// } // argc_argv_simple::option::operator bool
 
-bool argc_argv_simple::option::really_present() const
-{
-    return true;
+// bool argc_argv_simple::option::really_present() const
+// {
+//     return true;
 
-} // argc_argv_simple::option::really_present
+// } // argc_argv_simple::option::really_present
 
-argc_argv_simple::option::operator const char*() const
-{
-    if (!mValue)
-        throw option_has_no_value{std::string{mName} + " has no value"};
-    return mValue;
-
-} // argc_argv_simple::option::operator const char*
-
-// argc_argv_simple::option::operator std::string() const
+// argc_argv_simple::option::operator const char*() const
 // {
 //     if (!mValue)
 //         throw option_has_no_value{std::string{mName} + " has no value"};
 //     return mValue;
 
-// } // argc_argv_simple::option::operator std::string
+// } // argc_argv_simple::option::operator const char*
 
-argc_argv_simple::option::operator double() const
-{
-    if (!mValue)
-        throw option_has_no_value{std::string{mName} + " has no value"};
-    try {
-        return std::stod(mValue);
-    }
-    catch (std::exception&) {
-        throw option_value_conversion_failed{std::string{"cannot convert value of "} + mName + " to double: " + mValue};
-    }
+// // argc_argv_simple::option::operator std::string() const
+// // {
+// //     if (!mValue)
+// //         throw option_has_no_value{std::string{mName} + " has no value"};
+// //     return mValue;
 
-} // argc_argv_simple::option::operator double
+// // } // argc_argv_simple::option::operator std::string
 
-argc_argv_simple::option::operator int() const
-{
-    if (!mValue)
-        throw option_has_no_value{std::string{mName} + " has no value"};
-    try {
-        return std::stoi(mValue);
-    }
-    catch (std::exception&) {
-        throw option_value_conversion_failed{std::string{"cannot convert value of "} + mName + " to int: " + mValue};
-    }
+// argc_argv_simple::option::operator double() const
+// {
+//     if (!mValue)
+//         throw option_has_no_value{std::string{mName} + " has no value"};
+//     try {
+//         return std::stod(mValue);
+//     }
+//     catch (std::exception&) {
+//         throw option_value_conversion_failed{std::string{"cannot convert value of "} + mName + " to double: " + mValue};
+//     }
 
-} // argc_argv_simple::option::operator int
+// } // argc_argv_simple::option::operator double
 
-// ----------------------------------------------------------------------
+// argc_argv_simple::option::operator int() const
+// {
+//     if (!mValue)
+//         throw option_has_no_value{std::string{mName} + " has no value"};
+//     try {
+//         return std::stoi(mValue);
+//     }
+//     catch (std::exception&) {
+//         throw option_value_conversion_failed{std::string{"cannot convert value of "} + mName + " to int: " + mValue};
+//     }
 
-class no_option : public argc_argv_simple::option
-{
- public:
-    inline no_option(std::string name) : argc_argv_simple::option(name, nullptr) {}
-    operator bool() const override { return false; }
-    bool really_present() const override { return false; }
-    operator const char*() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
-    // operator std::string() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
-    operator double() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
-    operator int() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
+// } // argc_argv_simple::option::operator int
 
-}; // class no_option
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// class no_option : public argc_argv_simple::option
+// {
+//  public:
+//     inline no_option(std::string name) : argc_argv_simple::option(name, nullptr) {}
+//     operator bool() const override { return false; }
+//     bool really_present() const override { return false; }
+//     operator const char*() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
+//     // operator std::string() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
+//     operator double() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
+//     operator int() const override { throw argc_argv_simple::option_not_found{std::string{"option "} + name() + " not found in the command line"}; }
 
-class option_with_default : public argc_argv_simple::option
-{
- public:
-    inline option_with_default(std::string name, std::string value) : argc_argv_simple::option(name, nullptr), mSvalue(value) { set_value(mSvalue.data()); }
-    template <typename T> inline option_with_default(std::string name, T value) : argc_argv_simple::option(name, nullptr), mSvalue(std::to_string(value)) { set_value(mSvalue.data()); }
-    bool really_present() const override { return false; }
+// }; // class no_option
 
- private:
-    const std::string mSvalue;
+// // ----------------------------------------------------------------------
 
-}; // class no_option
+// class option_with_default : public argc_argv_simple::option
+// {
+//  public:
+//     inline option_with_default(std::string name, std::string value) : argc_argv_simple::option(name, nullptr), mSvalue(value) { set_value(mSvalue.data()); }
+//     template <typename T> inline option_with_default(std::string name, T value) : argc_argv_simple::option(name, nullptr), mSvalue(std::to_string(value)) { set_value(mSvalue.data()); }
+//     bool really_present() const override { return false; }
 
-// ----------------------------------------------------------------------
+//  private:
+//     const std::string mSvalue;
 
-argc_argv_simple::argc_argv_simple(int argc, const char* const argv[], std::initializer_list<const char*> options_with_value, bool split_single_dash)
-    : mOptionsWithValue{options_with_value}, mProgram{argv[0]}
-{
-    for (int arg_no = 1; arg_no < argc; ++arg_no) {
-        if (argv[arg_no][0] == '-') {
-            if (std::find_if(mOptionsWithValue.begin(), mOptionsWithValue.end(), [arg=argv[arg_no]](const char* aOption) { return !std::strcmp(arg, aOption); }) != mOptionsWithValue.end()) {
-                if ((arg_no + 1) >= argc)
-                    throw option_has_no_value{std::string{argv[arg_no]} + " requires a value, but it is not in the command line"};
-                mOptions.emplace_back(new option(argv[arg_no], argv[arg_no + 1]));
-                // std::cout << "Add option with value: " << argv[arg_no] << ' ' << argv[arg_no + 1] << '\n';
-                ++arg_no;
-            }
-            else if (argv[arg_no][1] != '-' && split_single_dash) {
-                for (const char* item = &argv[arg_no][1]; *item; ++item) {
-                    mOptions.emplace_back(new option({'-', *item}));
-                }
-            }
-            else {
-                mOptions.emplace_back(new option(argv[arg_no]));
-                // std::cout << "Add option: " << argv[arg_no] << '\n';
-            }
-        }
-        else {
-            mArguments.push_back(argv[arg_no]);
-        }
-    }
+// }; // class no_option
 
-} // argc_argv_simple::argc_argv_simple
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// argc_argv_simple::argc_argv_simple(int argc, const char* const argv[], std::initializer_list<const char*> options_with_value, bool split_single_dash)
+//     : mOptionsWithValue{options_with_value}, mProgram{argv[0]}
+// {
+//     for (int arg_no = 1; arg_no < argc; ++arg_no) {
+//         if (argv[arg_no][0] == '-') {
+//             if (std::find_if(mOptionsWithValue.begin(), mOptionsWithValue.end(), [arg=argv[arg_no]](const char* aOption) { return !std::strcmp(arg, aOption); }) != mOptionsWithValue.end()) {
+//                 if ((arg_no + 1) >= argc)
+//                     throw option_has_no_value{std::string{argv[arg_no]} + " requires a value, but it is not in the command line"};
+//                 mOptions.emplace_back(new option(argv[arg_no], argv[arg_no + 1]));
+//                 // std::cout << "Add option with value: " << argv[arg_no] << ' ' << argv[arg_no + 1] << '\n';
+//                 ++arg_no;
+//             }
+//             else if (argv[arg_no][1] != '-' && split_single_dash) {
+//                 for (const char* item = &argv[arg_no][1]; *item; ++item) {
+//                     mOptions.emplace_back(new option({'-', *item}));
+//                 }
+//             }
+//             else {
+//                 mOptions.emplace_back(new option(argv[arg_no]));
+//                 // std::cout << "Add option: " << argv[arg_no] << '\n';
+//             }
+//         }
+//         else {
+//             mArguments.push_back(argv[arg_no]);
+//         }
+//     }
 
-const argc_argv_simple::option& argc_argv_simple::operator[](std::string aName) const
-{
-    const auto found = std::find_if(mOptions.begin(), mOptions.end(), [&aName](const auto& opt) { return opt->name() == aName; });
-    if (found != mOptions.end())
-        return *found->get();
-    else
-        return *mOptions.emplace_back(new no_option(aName));
+// } // argc_argv_simple::argc_argv_simple
 
-} // argc_argv_simple::operator[]
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// const argc_argv_simple::option& argc_argv_simple::operator[](std::string aName) const
+// {
+//     const auto found = std::find_if(mOptions.begin(), mOptions.end(), [&aName](const auto& opt) { return opt->name() == aName; });
+//     if (found != mOptions.end())
+//         return *found->get();
+//     else
+//         return *mOptions.emplace_back(new no_option(aName));
 
-const argc_argv_simple::option& argc_argv_simple::get(std::string aName) const
-{
-    const auto found = std::find_if(mOptions.begin(), mOptions.end(), [&aName](const auto& opt) { return opt->name() == aName; });
-    if (found != mOptions.end()) {
-        if ((*found)->really_present()) // option was really present or default
-            return *found->get();
-        mOptions.erase(found);
-    }
-    throw option_not_found{""};
+// } // argc_argv_simple::operator[]
 
-} // argc_argv_simple::get
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// const argc_argv_simple::option& argc_argv_simple::get(std::string aName) const
+// {
+//     const auto found = std::find_if(mOptions.begin(), mOptions.end(), [&aName](const auto& opt) { return opt->name() == aName; });
+//     if (found != mOptions.end()) {
+//         if ((*found)->really_present()) // option was really present or default
+//             return *found->get();
+//         mOptions.erase(found);
+//     }
+//     throw option_not_found{""};
 
-const argc_argv_simple::option& argc_argv_simple::get(std::string aName, std::string aDefault) const
-{
-    try {
-        return get(aName);
-    }
-    catch (option_not_found&) {
-        return *mOptions.emplace_back(new option_with_default(aName, aDefault));
-    }
+// } // argc_argv_simple::get
 
-} // argc_argv_simple::get
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// const argc_argv_simple::option& argc_argv_simple::get(std::string aName, std::string aDefault) const
+// {
+//     try {
+//         return get(aName);
+//     }
+//     catch (option_not_found&) {
+//         return *mOptions.emplace_back(new option_with_default(aName, aDefault));
+//     }
 
-const argc_argv_simple::option& argc_argv_simple::get(std::string aName, double aDefault) const
-{
-    try {
-        return get(aName);
-    }
-    catch (option_not_found&) {
-        return *mOptions.emplace_back(new option_with_default(aName, aDefault));
-    }
+// } // argc_argv_simple::get
 
-} // argc_argv_simple::get
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// const argc_argv_simple::option& argc_argv_simple::get(std::string aName, double aDefault) const
+// {
+//     try {
+//         return get(aName);
+//     }
+//     catch (option_not_found&) {
+//         return *mOptions.emplace_back(new option_with_default(aName, aDefault));
+//     }
 
-const argc_argv_simple::option& argc_argv_simple::get(std::string aName, int aDefault) const
-{
-    try {
-        return get(aName);
-    }
-    catch (option_not_found&) {
-        return *mOptions.emplace_back(new option_with_default(aName, aDefault));
-    }
+// } // argc_argv_simple::get
 
-} // argc_argv_simple::get
+// // ----------------------------------------------------------------------
 
-// ----------------------------------------------------------------------
+// const argc_argv_simple::option& argc_argv_simple::get(std::string aName, int aDefault) const
+// {
+//     try {
+//         return get(aName);
+//     }
+//     catch (option_not_found&) {
+//         return *mOptions.emplace_back(new option_with_default(aName, aDefault));
+//     }
 
-const char* argc_argv_simple::operator[](size_t aIndex) const
-{
-    if (aIndex >= mArguments.size())
-        throw argument_not_found{"invalid argument index: " + std::to_string(aIndex) + ", total arguments in the command line: " + std::to_string(mArguments.size())};
-    return mArguments[aIndex];
+// } // argc_argv_simple::get
 
-} // argc_argv_simple::operator[]
+// // ----------------------------------------------------------------------
+
+// const char* argc_argv_simple::operator[](size_t aIndex) const
+// {
+//     if (aIndex >= mArguments.size())
+//         throw argument_not_found{"invalid argument index: " + std::to_string(aIndex) + ", total arguments in the command line: " + std::to_string(mArguments.size())};
+//     return mArguments[aIndex];
+
+// } // argc_argv_simple::operator[]
 
 // ----------------------------------------------------------------------
 /// Local Variables:
--- cc/argc-argv.hh
+++ cc/argc-argv.hh
@@ -19,8 +19,11 @@ class argc_argv
     class argument_not_found : public std::runtime_error { public: using std::runtime_error::runtime_error; };
     class option_value_conversion_failed : public std::runtime_error { public: using std::runtime_error::runtime_error; };
 
+    using string = const char*;
+    using strings = std::vector<string>;
+
     // using mandatory = std::variant<std::string, int, size_t, double, bool>;
-    using option_default = std::variant<bool, const char*, long, double>;
+    using option_default = std::variant<bool, string, strings, long, double>;
 
     class option : public std::pair<std::string, option_default>
     {
@@ -39,8 +42,10 @@ class argc_argv
             {
                 if (auto* b = std::get_if<bool>(&second); b)
                     return *b;
-                else if (auto* const * s = std::get_if<const char*>(&second); s)
-                    return *s != nullptr && **s != 0;
+                else if (auto* s1 = std::get_if<string>(&second); s1)
+                    return *s1 != nullptr && **s1 != 0;
+                else if (auto* s2 = std::get_if<strings>(&second); s2)
+                    return !s2->empty();
                 else
                     throw option_value_conversion_failed{std::string{"cannot convert value of "} + first + " to bool"};
             }
@@ -49,7 +54,8 @@ class argc_argv
         using std::pair<std::string, option_default>::pair;
 
         inline operator bool() const { return get_bool(); }
-        inline operator std::string() const { return get<const char*>(); }
+        inline operator std::string() const { return get<string>(); }
+        inline operator strings() const { return get<strings>(); }
         inline operator double() const { return get<double>(); }
         inline operator long() const { return get<long>(); }
         inline operator int() const { return static_cast<int>(get<long>()); }
@@ -94,72 +100,72 @@ inline bool operator != (const char* aStr, const argc_argv::option& aOpt) { retu
 
 // ----------------------------------------------------------------------
 
-class argc_argv_simple
-{
- public:
-    class option_not_found : public std::runtime_error { public: using std::runtime_error::runtime_error; };
-    class argument_not_found : public std::runtime_error { public: using std::runtime_error::runtime_error; };
-    class option_has_no_value : public std::runtime_error { public: using std::runtime_error::runtime_error; };
-    class option_value_conversion_failed : public std::runtime_error { public: using std::runtime_error::runtime_error; };
+// class argc_argv_simple
+// {
+//  public:
+//     class option_not_found : public std::runtime_error { public: using std::runtime_error::runtime_error; };
+//     class argument_not_found : public std::runtime_error { public: using std::runtime_error::runtime_error; };
+//     class option_has_no_value : public std::runtime_error { public: using std::runtime_error::runtime_error; };
+//     class option_value_conversion_failed : public std::runtime_error { public: using std::runtime_error::runtime_error; };
 
-    class option
-    {
-     public:
-        virtual ~option();
+//     class option
+//     {
+//      public:
+//         virtual ~option();
 
-          // if option was present in the command line
-        virtual operator bool() const;
+//           // if option was present in the command line
+//         virtual operator bool() const;
 
-          // argument extraction, throws option_not_found, option_has_no_value, option_value_conversion_failed
-        virtual operator const char*() const;
-        inline operator std::string() const { return operator const char*(); }
-        virtual operator double() const;
-        virtual operator int() const;
+//           // argument extraction, throws option_not_found, option_has_no_value, option_value_conversion_failed
+//         virtual operator const char*() const;
+//         inline operator std::string() const { return operator const char*(); }
+//         virtual operator double() const;
+//         virtual operator int() const;
 
-     protected:
-        inline option(std::string name, const char* value = nullptr) : mName{name}, mValue{value} {}
-        friend class argc_argv_simple;
+//      protected:
+//         inline option(std::string name, const char* value = nullptr) : mName{name}, mValue{value} {}
+//         friend class argc_argv_simple;
 
-        inline std::string name() const { return mName; }
-        inline void set_value(const char* aValue) { mValue = aValue; }
-        virtual bool really_present() const;
+//         inline std::string name() const { return mName; }
+//         inline void set_value(const char* aValue) { mValue = aValue; }
+//         virtual bool really_present() const;
 
-     private:
-        std::string mName;
-        const char* mValue;
+//      private:
+//         std::string mName;
+//         const char* mValue;
 
-    }; // class option
+//     }; // class option
 
-      // options_with_value: list of option names that have values, e.g. {"-o", "--output"}
-      // may throw option_has_no_value if last value in argv is option from options_with_value
-    argc_argv_simple(int argc, const char* const argv[], std::initializer_list<const char*> options_with_value, bool split_single_dash = true);
+//       // options_with_value: list of option names that have values, e.g. {"-o", "--output"}
+//       // may throw option_has_no_value if last value in argv is option from options_with_value
+//     argc_argv_simple(int argc, const char* const argv[], std::initializer_list<const char*> options_with_value, bool split_single_dash = true);
 
-      // returns option by name, if option was not in argv, returns ref to special option object
-    const option& operator[](std::string aName) const;
+//       // returns option by name, if option was not in argv, returns ref to special option object
+//     const option& operator[](std::string aName) const;
 
-      // returns option by name, if option was not in argv, returns ref to an option object having aDefault as a value
-    const option& get(std::string aName, std::string aDefault) const;
-    const option& get(std::string aName, double aDefault) const;
-    const option& get(std::string aName, int aDefault) const;
+//       // returns option by name, if option was not in argv, returns ref to an option object having aDefault as a value
+//     const option& get(std::string aName, std::string aDefault) const;
+//     const option& get(std::string aName, double aDefault) const;
+//     const option& get(std::string aName, int aDefault) const;
 
-      // returns number of arguments in the command line that are neither options nor option values
-    inline size_t number_of_arguments() const { return mArguments.size(); }
+//       // returns number of arguments in the command line that are neither options nor option values
+//     inline size_t number_of_arguments() const { return mArguments.size(); }
 
-      // returns argument (neither option nor option value) by index, throws argument_not_found if aIndex >= number_of_arguments()
-    const char* operator[](size_t aIndex) const;
+//       // returns argument (neither option nor option value) by index, throws argument_not_found if aIndex >= number_of_arguments()
+//     const char* operator[](size_t aIndex) const;
 
-      // returns argv[0]
-    inline const char* program() const { return mProgram; }
+//       // returns argv[0]
+//     inline const char* program() const { return mProgram; }
 
- private:
-    const std::vector<const char*> mOptionsWithValue;
-    const char* mProgram;
-    std::vector<const char*> mArguments;
-    mutable std::vector<std::unique_ptr<option>> mOptions;
+//  private:
+//     const std::vector<const char*> mOptionsWithValue;
+//     const char* mProgram;
+//     std::vector<const char*> mArguments;
+//     mutable std::vector<std::unique_ptr<option>> mOptions;
 
-    const option& get(std::string aName) const;
+//     const option& get(std::string aName) const;
 
-}; // class argc_argv_simple
+// }; // class argc_argv_simple
 
 // ----------------------------------------------------------------------
 /// Local Variables:
