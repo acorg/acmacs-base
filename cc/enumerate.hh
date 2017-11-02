93d8279aecb8a3112a71bc96cfa8e7e455eab6d7
Eugene Skepner <github@skepner.eu>
2017-11-02 12:08:32 +0100

    minor enumerate fixes

--- cc/enumerate.hh
+++ cc/enumerate.hh
@@ -7,64 +7,65 @@
 
 // ----------------------------------------------------------------------
 
-namespace _enumerate_internal
+namespace acmacs
 {
-    template <typename Iterator, typename index_type> struct enumerate_iterator
+    namespace _enumerate_internal
     {
-        using iterator = Iterator;
-          //using index_type = typename std::iterator_traits<iterator>::difference_type;
-        using reference = typename std::iterator_traits<iterator>::reference;
+        template <typename Iterator, typename index_type> struct enumerate_iterator
+        {
+            using iterator = Iterator;
+              //using index_type = typename std::iterator_traits<iterator>::difference_type;
+            using reference = typename std::iterator_traits<iterator>::reference;
 
-        enumerate_iterator(index_type aIndex, iterator aIterator) : index(aIndex), iter(aIterator) {}
-        enumerate_iterator& operator++() { ++index; ++iter; return *this; }
-        bool operator!=(const enumerate_iterator &other) const { return iter != other.iter; }
-        std::pair<index_type&, reference> operator*() { return {index, *iter}; }
+            inline enumerate_iterator(index_type aIndex, iterator aIterator) : index(aIndex), iter(aIterator) {}
+            inline enumerate_iterator& operator++() { ++index; ++iter; return *this; }
+            inline bool operator!=(const enumerate_iterator &other) const { return iter != other.iter; }
+            inline std::pair<index_type&, reference> operator*() { return {index, *iter}; }
 
-     private:
-        index_type index;
-        iterator iter;
-    };
+         private:
+            index_type index;
+            iterator iter;
+        };
 
-      // ----------------------------------------------------------------------
+          // ----------------------------------------------------------------------
 
-    template <typename Iterator, typename index_type> struct enumerate_range
-    {
-        // using index_type = typename std::iterator_traits<Iterator>::difference_type;
-        using iterator = enumerate_iterator<Iterator, index_type>;
+        template <typename Iterator, typename index_type> struct enumerate_range
+        {
+              // using index_type = typename std::iterator_traits<Iterator>::difference_type;
+            using iterator = enumerate_iterator<Iterator, index_type>;
 
-        enumerate_range(Iterator aFirst, Iterator aLast, index_type aInitial) : first(aFirst), last(aLast), initial(aInitial) {}
-        iterator begin() const { return iterator(initial, first); }
-        iterator end() const { return iterator(0, last); }
+            inline enumerate_range(Iterator aFirst, Iterator aLast, index_type aInitial) : first(aFirst), last(aLast), initial(aInitial) {}
+            inline iterator begin() const { return iterator(initial, first); }
+            inline iterator end() const { return iterator(0, last); }
 
-     private:
-        Iterator first;
-        Iterator last;
-        index_type initial;
-    };
+         private:
+            Iterator first;
+            Iterator last;
+            index_type initial;
+        };
 
-} // namespace _enumerate_internal
+    } // namespace _enumerate_internal
 
 // ----------------------------------------------------------------------
 
-namespace acmacs
-{
-    template <typename Iterator, typename index_type> auto enumerate(Iterator first, Iterator last, index_type initial = 0)
+    template <typename Iterator, typename index_type> inline auto enumerate(Iterator first, Iterator last, index_type initial = 0)
     {
         return _enumerate_internal::enumerate_range<Iterator, index_type>(first, last, initial);
     }
 
-    template <typename Container> auto enumerate(Container& content)
+    template <typename Container> inline auto enumerate(Container& content)
     {
         using iter_type = decltype(std::begin(content));
         return _enumerate_internal::enumerate_range<iter_type, size_t>(std::begin(content), std::end(content), 0U);
     }
 
-    template <typename Container> auto enumerate(const Container& content)
+    template <typename Container> inline auto enumerate(const Container& content)
     {
         using iter_type = decltype(std::begin(content));
         return _enumerate_internal::enumerate_range<iter_type, size_t>(std::begin(content), std::end(content), 0U);
     }
-}
+
+} // namespace acmacs
 
 // ----------------------------------------------------------------------
 /// Local Variables:
