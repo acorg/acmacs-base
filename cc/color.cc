506ca3f967ba75f113ae5ae710c8cd5ab12c9410
Eugene Skepner <github@skepner.eu>
2017-12-05 17:42:48 +0100

    minor fix

--- cc/color.cc
+++ cc/color.cc
@@ -45,6 +45,7 @@ std::string Color::to_hex_string() const
 
 void Color::from_string(const std::string_view& aColor)
 {
+    using namespace std::string_literals;
     if (aColor.empty())
         THROW_OR_CERR(std::invalid_argument("cannot read Color from empty string"));
     if (aColor[0] == '#') {
@@ -58,7 +59,7 @@ void Color::from_string(const std::string_view& aColor)
               mColor = v;
               break;
           default:
-              THROW_OR_CERR(std::invalid_argument("cannot read Color from " + aColor));
+              THROW_OR_CERR(std::invalid_argument("cannot read Color from "s + aColor));
         }
     }
     else {
@@ -66,7 +67,7 @@ void Color::from_string(const std::string_view& aColor)
         if (e != _internal::sNameToColor.end())
             mColor = e->second;
         else
-            THROW_OR_CERR(std::invalid_argument("cannot read Color from " + aColor));
+            THROW_OR_CERR(std::invalid_argument("cannot read Color from "s + aColor));
     }
 
 } // Color::from_string
