497a0b32a7b575f2b79732c1709fc203dbcb71dc
Eugene Skepner <github@skepner.eu>
2018-04-09 13:21:48 +0200

    Date fixed. days_between_dates()

--- cc/date.hh
+++ cc/date.hh
@@ -18,66 +18,76 @@ class Date
  public:
     enum Today { Today };
 
-    inline Date() : mDate() {}
-    inline Date(enum Today) : mDate(boost::gregorian::day_clock::local_day()) {}
-    inline Date(std::string aText) : mDate(boost::gregorian::from_string(aText)) {}
-    inline Date(std::string_view aText) : mDate(boost::gregorian::from_string(std::string(aText))) {}
-    inline Date(const char* aText) : mDate(boost::gregorian::from_string(std::string(aText))) {}
-    inline Date(boost::gregorian::date aBoostDate) : mDate(aBoostDate) {}
-    inline static Date today() { return Today; }
-    inline static Date months_ago(int number_of_months) { return Date(Today).decrement_month(number_of_months); }
-    inline static Date years_ago(int number_of_years) { return Date(Today).decrement_month(number_of_years * 12); }
-    inline static Date weeks_ago(int number_of_weeks) { return Date(Today).decrement_week(number_of_weeks); }
+    Date() : mDate() {}
+    Date(enum Today) : mDate(boost::gregorian::day_clock::local_day()) {}
+    template <typename S> Date(S&& aText) : mDate(from_string(std::string(std::forward<S>(aText)))) {}
+    Date(boost::gregorian::date aBoostDate) : mDate(aBoostDate) {}
+    static Date today() { return Today; }
+    static Date months_ago(int number_of_months) { return Date(Today).decrement_month(number_of_months); }
+    static Date years_ago(int number_of_years) { return Date(Today).decrement_month(number_of_years * 12); }
+    static Date weeks_ago(int number_of_weeks) { return Date(Today).decrement_week(number_of_weeks); }
 
-    inline Date& operator=(std::string aText) { if (!aText.empty()) mDate = boost::gregorian::from_string(aText); return *this; }
-    inline Date& operator=(const char* aText) { if (aText && *aText) mDate = boost::gregorian::from_string(aText); return *this; }
+    Date& operator=(std::string aText) { if (!aText.empty()) mDate = from_string(aText); return *this; }
+    Date& operator=(const char* aText) { if (aText && *aText) mDate = from_string(aText); return *this; }
 
-    inline bool operator < (const Date& d) const { return mDate < d.mDate; }
-    inline bool operator == (const Date& d) const { return mDate == d.mDate; }
-    inline bool operator >= (const Date& d) const { return mDate >= d.mDate; }
+    bool operator < (const Date& d) const { return mDate < d.mDate; }
+    bool operator == (const Date& d) const { return mDate == d.mDate; }
+    bool operator >= (const Date& d) const { return mDate >= d.mDate; }
 
-    inline bool empty() const { return mDate.is_not_a_date(); }
-    inline operator bool() const { return !mDate.is_not_a_date(); }
-    inline int year() const { return mDate.year(); }
-    inline int month() const { return mDate.month(); }
-    inline int day() const { return mDate.day(); }
+    bool empty() const { return mDate.is_not_a_date(); }
+    operator bool() const { return !mDate.is_not_a_date(); }
+    int year() const { return mDate.year(); }
+    int month() const { return mDate.month(); }
+    int day() const { return mDate.day(); }
 
-    inline std::string display() const { return to_iso_extended_string(mDate); }
-    inline operator std::string() const { return display(); }
+    std::string display() const { return to_iso_extended_string(mDate); }
+    operator std::string() const { return display(); }
 
       // returns date for the 1st day of the year-month stored in this
-    inline Date beginning_of_month() const { return boost::gregorian::date(mDate.year(), mDate.month(), 1); }
-    inline Date beginning_of_year() const { return boost::gregorian::date(mDate.year(), 1, 1); }
-    inline Date beginning_of_week() const { return boost::gregorian::first_day_of_the_week_before(boost::gregorian::Monday).get_date(mDate); }
+    Date beginning_of_month() const { return boost::gregorian::date(mDate.year(), mDate.month(), 1); }
+    Date beginning_of_year() const { return boost::gregorian::date(mDate.year(), 1, 1); }
+    Date beginning_of_week() const { return boost::gregorian::first_day_of_the_week_before(boost::gregorian::Monday).get_date(mDate); }
 
-    inline Date& increment_month(int number_of_months = 1) { mDate += boost::gregorian::months(number_of_months); return *this; }
-    inline Date& decrement_month(int number_of_months = 1) { mDate -= boost::gregorian::months(number_of_months); return *this; }
-    inline Date& increment_year(int number_of_years = 1) { mDate += boost::gregorian::years(number_of_years); return *this; }
-    inline Date& decrement_year(int number_of_years = 1) { mDate -= boost::gregorian::years(number_of_years); return *this; }
-    inline Date& increment_week(int number_of_weeks = 1) { mDate += boost::gregorian::weeks(number_of_weeks); return *this; }
-    inline Date& decrement_week(int number_of_weeks = 1) { mDate -= boost::gregorian::weeks(number_of_weeks); return *this; }
+    Date& increment_month(int number_of_months = 1) { mDate += boost::gregorian::months(number_of_months); return *this; }
+    Date& decrement_month(int number_of_months = 1) { mDate -= boost::gregorian::months(number_of_months); return *this; }
+    Date& increment_year(int number_of_years = 1) { mDate += boost::gregorian::years(number_of_years); return *this; }
+    Date& decrement_year(int number_of_years = 1) { mDate -= boost::gregorian::years(number_of_years); return *this; }
+    Date& increment_week(int number_of_weeks = 1) { mDate += boost::gregorian::weeks(number_of_weeks); return *this; }
+    Date& decrement_week(int number_of_weeks = 1) { mDate -= boost::gregorian::weeks(number_of_weeks); return *this; }
 
-    inline Date next_month() const { return mDate + boost::gregorian::months(1); }
-    inline Date next_year() const { return mDate + boost::gregorian::years(1); }
-    inline Date next_week() const { return mDate + boost::gregorian::weeks(1); }
+    Date next_month() const { return mDate + boost::gregorian::months(1); }
+    Date next_year() const { return mDate + boost::gregorian::years(1); }
+    Date next_week() const { return mDate + boost::gregorian::weeks(1); }
 
-    inline std::string month_3() const { return format("%b"); }
-    inline std::string year_2() const { return format("%y"); }
-    inline std::string year_4() const { return format("%Y"); }
-    inline std::string month3_year2() const { return format("%b %y"); }
-    inline std::string monthtext_year() const { return format("%B %Y"); }
-    inline std::string year4_month2() const { return format("%Y-%m"); }
-    inline std::string year4_month2_day2() const { return format("%Y-%m-%d"); }
+    std::string month_3() const { return format("%b"); }
+    std::string year_2() const { return format("%y"); }
+    std::string year_4() const { return format("%Y"); }
+    std::string month3_year2() const { return format("%b %y"); }
+    std::string monthtext_year() const { return format("%B %Y"); }
+    std::string year4_month2() const { return format("%Y-%m"); }
+    std::string year4_month2_day2() const { return format("%Y-%m-%d"); }
+
+    const auto& gregorian() const { return mDate; }
 
  private:
     boost::gregorian::date mDate;
 
-    inline std::string format(const char* fmt) const
+    std::string format(const char* fmt) const
         {
             std::ostringstream stream;
             stream.imbue(std::locale(std::locale::classic(), new boost::gregorian::date_facet(fmt)));
             stream << mDate;
             return stream.str();
+        }
+
+    static inline boost::gregorian::date from_string(std::string source)
+        {
+            try {
+                return boost::gregorian::from_string(source);
+            }
+            catch (std::exception&) {
+                return boost::gregorian::from_undelimited_string(source);
+            }
         }
 
 }; // class Date
@@ -166,6 +176,28 @@ inline int weeks_between_dates(const Date& a, const Date& b)
 inline int weeks_between_dates(const std::pair<Date, Date>& aDates)
 {
     return weeks_between_dates(aDates.first, aDates.second);
+}
+
+// ----------------------------------------------------------------------
+
+// returns negative if b is earlier than a
+inline ssize_t days_between_dates(const Date& a, const Date& b)
+{
+    ssize_t days = 0;
+    if (a && b) {
+        if (b < a) {
+            days = - days_between_dates(b, a);
+        }
+        else {
+            try {
+                days = (b.gregorian() - a.gregorian()).days();
+            }
+            catch (std::exception& err) {
+                throw std::runtime_error("Cannot find days between \"" + a.display() + "\" and \"" + b.display() + "\": " + err.what());
+            }
+        }
+    }
+    return days;
 }
 
 // ----------------------------------------------------------------------
