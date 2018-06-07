02f79f4d12446f476ac628d26cea750598fba81b
Eugene Skepner <github@skepner.eu>
2018-06-07 10:41:41 +0200

    elapsed time formatting improvement

--- cc/timeit.cc
+++ cc/timeit.cc
@@ -20,9 +20,7 @@ std::string acmacs::format(acmacs::duration_t duration)
     const auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration % std::chrono::hours(1)).count();
     format_val(result, minutes, ':');
     const auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration % std::chrono::minutes(1)).count();
-    if (result.tellg())
-        result << std::setw(2) << std::setfill('0');
-    result << seconds << '.';
+    result << std::setw(2) << std::setfill('0') << seconds << '.';
     const auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(duration % std::chrono::seconds(1)).count();
     result << std::setw(6) << std::setfill('0') << microseconds;
     return result.str();
