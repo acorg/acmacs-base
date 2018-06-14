717b93415f8c917f394c57f81ce02ff6743db5cf
Eugene Skepner <github@skepner.eu>
2018-06-14 14:57:23 +0200

    acmacs::LineSide

--- cc/line.hh
+++ cc/line.hh
@@ -13,7 +13,9 @@ namespace acmacs
     {
      public:
         LineDefinedByEquation() = default;
+        LineDefinedByEquation(const LineDefinedByEquation&) = default;
         LineDefinedByEquation(double slope, double intercept) : slope_{slope}, intercept_{intercept} {}
+        LineDefinedByEquation& operator=(const LineDefinedByEquation&) = default;
 
         constexpr double slope() const { return slope_; }
         constexpr double intercept() const { return intercept_; }
@@ -46,6 +48,34 @@ namespace acmacs
     }; // class LineDefinedByEquation
 
     inline std::ostream& operator<<(std::ostream& out, const LineDefinedByEquation& line) { return out << "Line(slope:" << line.slope() << ", intercept:" << line.intercept() << ')'; }
+
+// ----------------------------------------------------------------------
+
+    class LineSide : public LineDefinedByEquation
+    {
+     public:
+        enum class side { negative, positive };
+
+        LineSide() = default;
+        LineSide(double slope, double intercept, side a_side) : LineDefinedByEquation(slope, intercept), side_{a_side} {}
+        LineSide(const LineDefinedByEquation& line, side a_side) : LineDefinedByEquation(line), side_{a_side} {}
+
+          // if passed point is one the correct side, leaves it as is,
+          // otherwise flips it to the correct side
+        template <typename V> V fix(const V& source) const
+            {
+                if (const auto dist = distance_with_direction(source); (dist * side_sign()) < 0)
+                    return flip_over(source);
+                else
+                    return source;
+            }
+
+     private:
+        side side_ = side::positive;
+
+        constexpr double side_sign() const { return side_ == side::negative ? -1.0 : 1.0; }
+
+    }; // class LineSide
 
 } // namespace acmacs
 
