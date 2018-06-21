89720ce11a355d86013363b069bf2898fa6ad9e7
Eugene Skepner <github@skepner.eu>
2018-06-21 09:14:21 +0200

    hint for setting ACMACSD_ROOT

--- cc/acmacsd.hh
+++ cc/acmacsd.hh
@@ -13,7 +13,7 @@ namespace acmacs
         using namespace std::string_literals;
         if (auto root = std::getenv("ACMACSD_ROOT"); root)
             return root;
-        throw std::runtime_error("ACMACSD_ROOT env is not set");
+        throw std::runtime_error("ACMACSD_ROOT env is not set (for apache-mod-acmacs see /etc/apache2/envvars)");
     }
 
 } // namespace acmacs
