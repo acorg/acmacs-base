338e956c23a4e25ce2fb5b3507a2aad46f5f0cfb
Eugene Skepner <github@skepner.eu>
2018-10-04 16:43:27 +0200

    acmacs::CsvWriter improvements

--- cc/csv.hh
+++ cc/csv.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "acmacs-base/string.hh"
+#include "acmacs-base/sfinae.hh"
 
 // ----------------------------------------------------------------------
 
@@ -8,29 +9,59 @@ namespace acmacs
 {
     class CsvWriter
     {
-     public:
-        inline operator std::string() const { return mData; }
-        inline operator std::string_view() const { return mData; }
+      public:
+        operator std::string() const { return mData; }
+        operator std::string_view() const { return mData; }
 
-        inline void new_row() { mData.append(1, '\n'); }
+        CsvWriter& new_row()
+        {
+            mData.append(1, '\n');
+            return *this;
+        }
 
-        inline void add_field(std::string aField)
-            {
-                  // https://www.ietf.org/rfc/rfc4180.txt
-                if (aField.find(',') != std::string::npos || aField.find('\n') != std::string::npos || aField.find('"') != std::string::npos) {
-                    aField = '"' + string::replace(aField, "\"", "\"\"") + '"';
-                }
-                if (!mData.empty() && mData.back() != '\n')
-                    mData.append(1, ',');
-                mData.append(aField);
+        void add_field(std::string aField)
+        {
+            // https://www.ietf.org/rfc/rfc4180.txt
+            if (aField.find(',') != std::string::npos || aField.find('\n') != std::string::npos || aField.find('"') != std::string::npos) {
+                aField = '"' + string::replace(aField, "\"", "\"\"") + '"';
             }
+            if (!mData.empty() && mData.back() != '\n')
+                mData.append(1, ',');
+            mData.append(aField);
+        }
 
-        inline void add_empty_field() { if (!mData.empty() && mData.back() != '\n') mData.append(1, ','); }
+        void add_empty_field()
+        {
+            if (!mData.empty() && mData.back() != '\n')
+                mData.append(1, ',');
+        }
 
-     private:
+        CsvWriter& field()
+        {
+            add_empty_field();
+            return *this;
+        }
+
+        template <typename S, typename=sfinae::string_only_t<S>> CsvWriter& field(S field)
+        {
+            add_field(to_string(field));
+            return *this;
+        }
+
+        template <typename S, typename=sfinae::string_only_t<S>> CsvWriter& operator<<(S data) { return field(data); }
+
+        enum empty_field_ { empty_field };
+        CsvWriter& operator<<(empty_field_) { return field(); }
+
+        enum end_of_row_ { end_of_row };
+        CsvWriter& operator<<(end_of_row_) { return new_row(); }
+
+      private:
         std::string mData;
 
     }; // class CsvWriter
+
+    inline std::ostream& operator<<(std::ostream& out, const CsvWriter& csv) { return out << static_cast<std::string_view>(csv); }
 
 } // namespace acmacs
 
