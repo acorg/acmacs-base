5a3e2c3d98bf3a06321d9dd092464edf5e8fcb94
Eugene Skepner <github@skepner.eu>
2018-11-12 09:27:07 +0100

    acmacs::file::istream improvements

--- cc/read-file.hh
+++ cc/read-file.hh
@@ -88,13 +88,14 @@ namespace acmacs::file
             }
         ifstream(std::string_view filename) : ifstream(std::string(filename)) {}
 
-        std::istream& stream()
-            {
-                return std::visit([](auto&& stream) -> std::istream& { return stream; }, backend_);
-            }
+        std::istream& stream() { return std::visit([](auto&& stream) -> std::istream& { return stream; }, backend_); }
+        const std::istream& stream() const { return std::visit([](auto&& stream) -> const std::istream& { return stream; }, backend_); }
 
+        std::istream& operator*() { return stream(); }
+        std::istream* operator->() { return &stream(); }
         operator std::istream&() { return stream(); }
         std::string read() { return {std::istreambuf_iterator<char>(stream()), {}}; }
+        explicit operator bool() const { return bool(stream()); }
 
      private:
         std::variant<std::reference_wrapper<std::istream>,std::ifstream> backend_;
@@ -112,13 +113,13 @@ namespace acmacs::file
             }
         ofstream(std::string_view filename) : ofstream(std::string(filename)) {}
 
-        std::ostream& stream()
-            {
-                return std::visit([](auto&& stream) -> std::ostream& { return stream; }, backend_);
-            }
+        std::ostream& stream() { return std::visit([](auto&& stream) -> std::ostream& { return stream; }, backend_); }
+        const std::ostream& stream() const { return std::visit([](auto&& stream) -> const std::ostream& { return stream; }, backend_); }
 
         std::ostream& operator*() { return stream(); }
+        std::ostream* operator->() { return &stream(); }
         operator std::ostream&() { return stream(); }
+        explicit operator bool() const { return bool(stream()); }
 
      private:
         std::variant<std::reference_wrapper<std::ostream>,std::ofstream> backend_;
