aa31f7af04970d669a8da90d945b2f3584825460
Eugene Skepner <github@skepner.eu>
2019-05-14 19:07:44 +0200

    virus name parsing refactoring

--- cc/virus-name.cc
+++ cc/virus-name.cc
@@ -6,6 +6,7 @@
 
 #include "acmacs-base/virus-name.hh"
 
+
 // ----------------------------------------------------------------------
 
 #include "acmacs-base/global-constructors-push.hh"
@@ -23,6 +24,177 @@ static const std::array sReReassortant = {
 
 namespace virus_name
 {
+    static void split_and_strip(std::string name, std::string& virus_type, std::string& host, std::string& location, std::string& isolation, std::string& year, std::string& extra);
+
+    // // Extracts virus name without passage, reassortant, extra,
+    // // etc. and calculates match threshold (to use with
+    // // acmacs_chart::Antigens::find_by_name_matching), match threshold is a square
+    // // of virus name length.
+    // static size_t match_threshold(std::string name);
+
+    namespace _internal
+    {
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wglobal-constructors"
+#pragma GCC diagnostic ignored "-Wexit-time-destructors"
+#endif
+
+        const std::regex cdc_name{"^([A-Z][A-Z][A-Z]?)[ \\-]"}; // cdc names had location abbreviation separated with '-' earlier
+
+        // const std::string flu_a_subtype{"H[1-9][0-9]?(?:N[1-9][0-9]?)?"};
+        // const std::string flu_type_subtype("(?:B|A\\s*[\\(/]?" + flu_a_subtype + "\\)?)");
+        // const std::string flu_host{"[A-Z\\s]+"};
+        // const std::string flu_location{"[A-Z\\-_\\s0-9]+"};
+        // const std::string flu_isolation{"[^/]+"};
+        // const std::string flu_year{""};
+
+        constexpr const char* re_international_name =
+                "^(?:([AB][^/]*)/)?" // type subtype
+                "(?:([^/]+)/)?"      // host
+                "([^/]{2,})/"        // location/
+                "0*([^/]+)/"         // isolation /
+                "(19|20)?(\\d\\d)"   // year
+                "(?:\\s*\\)\\s*)?"   // closing ) surrounded with spaces
+                ;
+
+        constexpr const char* re_reassortant_passage = "(?:(?:\\s+|__)(.+))?";
+
+          // [1] - type+subtype, [2] - host, [3] - location, [4] - isolation-number (omitting leading zeros), [5] - century, [6] - year (2 last digit), [7] - reassortant and passage
+          // const std::regex international{"^([AB][^/]*)/(?:([^/]+)/)?([^/]+)/0*([^/]+)/(19|20)?(\\d\\d)(?:(?:\\s+|__)(.+))?$"};
+        const std::regex international{std::string(re_international_name) + re_reassortant_passage + '$'};
+        const std::regex international_name{re_international_name};
+        const std::regex international_name_with_extra{std::string(re_international_name) + "(?:[\\s_\\-]*(.+))?"};
+
+        const std::regex passage_after_name{" (MDCK|SIAT|MK|E|X)[X\\?\\d]"}; // to extract cdc name only! NOT to extract passage!
+
+#pragma GCC diagnostic pop
+
+        inline std::string make_year(const std::smatch& m)
+        {
+            std::string year;
+            if (m[5].length())
+                year = m[5].str() + m[6].str();
+            else if (m[6].str()[0] > '2')
+                year = "19" + m[6].str();
+            else
+                year = "20" + m[6].str();
+            return year;
+        }
+
+        // constexpr const size_t international_name_suffix_size = 9; // len(lo/isolation-number/year) >= 9
+
+        inline std::string location_human(std::string name, size_t prefix_size)
+        {
+            const auto end = name.find('/', prefix_size);
+            return name.substr(prefix_size, end - prefix_size);
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
+    std::string_view name(std::string_view aFullName)
+    {
+        std::cmatch m;
+        if (std::regex_search(aFullName.begin(), aFullName.end(), m, _internal::international_name)) {
+            return aFullName.substr(0, static_cast<size_t>(m.length()));
+        }
+        else if (std::regex_search(aFullName.begin(), aFullName.end(), m, _internal::passage_after_name)) { // works for cdc name without extra and without reassortant (cdc names usually do not have reassortant)
+            return aFullName.substr(0, static_cast<size_t>(m.position()));
+        }
+        else {
+            return aFullName;   // failed to split, perhaps cdc name without passage
+        }
+    }
+
+    // ----------------------------------------------------------------------
+
+    std::string location_for_cdc_name(std::string name)
+    {
+        std::smatch m;
+        if (std::regex_search(name, m, _internal::cdc_name))
+            return "#" + m[1].str();
+        throw Unrecognized{"No cdc abbreviation in " + name};
+    }
+
+    // ----------------------------------------------------------------------
+
+    std::string location(std::string name, prioritize_cdc_name check_cdc_first)
+    {
+        try {
+            if (check_cdc_first == prioritize_cdc_name::yes)
+                return location_for_cdc_name(name);
+        }
+        catch (Unrecognized&) {
+        }
+
+        std::smatch m;
+        if (std::regex_search(name, m, _internal::international_name)) // international name with possible "garbage" after year, e.g. A/TOKYO/UT-IMS2-1/2014_HY-PR8-HA-N121K
+            return m[3].str();
+
+        try {
+            if (check_cdc_first == prioritize_cdc_name::no)
+                return location_for_cdc_name(name);
+        }
+        catch (Unrecognized&) {
+        }
+
+        throw Unrecognized{"No location in " + name};
+    }
+
+    // // Faster version of location() for A(H3N2)/ and A(H1N1)/ names without host field
+    // inline std::string location_human_a(std::string name)
+    // {
+    //     constexpr const size_t prefix_size = 8;
+    //     return (name.size() > (prefix_size + _internal::international_name_suffix_size) && name[prefix_size - 1] == '/') ? _internal::location_human(name, prefix_size) : location(name);
+    // }
+
+    // // Faster version of location() for B/ names without host field
+    // inline std::string location_human_b(std::string name)
+    // {
+    //     constexpr const size_t prefix_size = 2;
+    //     return (name.size() > (prefix_size + _internal::international_name_suffix_size) && name[prefix_size - 1] == '/') ? _internal::location_human(name, prefix_size) : location(name);
+    // }
+
+    // ----------------------------------------------------------------------
+
+    std::string year(std::string name)
+    {
+        std::smatch m;
+        if (std::regex_search(name, m, _internal::international_name))
+            return _internal::make_year(m);
+        throw Unrecognized("No year in " + name);
+
+    } // year
+
+    // ----------------------------------------------------------------------
+
+    // size_t match_threshold(std::string name)
+    // {
+    //     size_t result = 0;
+    //     std::smatch m;
+    //     if (std::regex_search(name, m, _internal::international_name)) {
+    //         // find end of year (m[6])
+    //         const auto end_of_year_offset = static_cast<size_t>(m[6].second - name.begin());
+    //         result = end_of_year_offset * end_of_year_offset;
+    //         // std::cerr << "INFO: match_threshold: end_of_year_offset:" << end_of_year_offset << " name:" << name << std::endl;
+    //     }
+    //     return result;
+    // }
+
+    // ----------------------------------------------------------------------
+
+    std::string_view virus_type(const std::string& name) // pass by reference! because we return string_view to it
+    {
+        std::smatch m;
+        if (std::regex_search(name, m, _internal::international_name))
+            return {name.data() + m.position(1), static_cast<size_t>(m.length(1))};
+        throw Unrecognized("No virus_type in " + name);
+
+    } // AntigenSerum::virus_type
+
+    // ----------------------------------------------------------------------
+
     void split(std::string name, std::string& virus_type, std::string& host, std::string& location, std::string& isolation, std::string& year, std::string& passage)
     {
         std::smatch m;
