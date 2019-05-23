9a50bc8b22f9791bd6a2491b49464ba4c8946a8e
Eugene Skepner <github@skepner.eu>
2019-05-23 10:04:43 +0200

    fmt lib

---
+++ cc/fmt.hh
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma GCC diagnostic ignored "-Wundefined-func-template"
+#endif
+
+#include <fmt/format.h>
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
