e6708ba5975a1fedcdbcdd1633d767399d1c63bf
Eugene Skepner <github@skepner.eu>
2019-05-24 09:08:14 +0200

    fmt for std::exception

--- cc/fmt.hh
+++ cc/fmt.hh
@@ -12,6 +12,14 @@
 #pragma GCC diagnostic pop
 
 // ----------------------------------------------------------------------
+
+template <> struct fmt::formatter<std::exception>
+{
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
+    template <typename FormatContext> auto format(const std::exception& err, FormatContext& ctx) { return format_to(ctx.out(), "{}", err.what()); }
+};
+
+// ----------------------------------------------------------------------
 /// Local Variables:
 /// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
 /// End:
