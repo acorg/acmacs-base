0fcfd621c96e008978619cef965c30bf9b6e3d60
Eugene Skepner <github@skepner.eu>
2019-05-26 09:05:04 +0200

    range-v3

---
+++ cc/range-v3.hh
@@ -0,0 +1,21 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wshadow"
+#endif
+
+// #include <range/v3/core.hpp>
+// #include <range/v3/algorithm.hpp>
+
+#include <range/v3/all.hpp>
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
