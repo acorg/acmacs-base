adfe07995a9eb8ecc638d831e357977a3d923642
Eugene Skepner <github@skepner.eu>
2019-06-18 11:01:46 +0200

    to_json::v2

--- cc/test-to-json.cc
+++ cc/test-to-json.cc
@@ -4,13 +4,6 @@
 
 int main()
 {
-    // {
-    //     auto js = to_json::v1_5::object();
-    //     fmt::print("{}\n\n", js);
-    //     js << to_json::v1_5::key_val{"ARR2", to_json::v1_5::array(1, 2)};
-    //     fmt::print("{}\n", js);
-    // }
-
     {
         auto js = to_json::v2::object();
         js << to_json::v2::key_val("S1", "A") << to_json::v2::key_val("N1", 7.3);
@@ -29,14 +22,6 @@ int main()
            ;
         fmt::print("{}\n\n", js);
     }
-
-    // {
-    //     auto js = to_json::object();
-    //     js << to_json::key_val{"ARR2", to_json::array(1, 2)} << to_json::key_val{"ARR3", to_json::array(1, 2, "x")}
-    //        << to_json::key_val{"OBJ1", to_json::object(to_json::key_val{"s1", to_json::value("s1")}, to_json::key_val{"s2", to_json::value("s2")})};
-    //     // 11.2)} << to_json::key_val{"aa", to_json::value("xx")};
-    //     fmt::print("{}\n", js);
-    // }
 
     return 0;
 }
