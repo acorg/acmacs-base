771ed32b4035b017755e1c626d7646464d3f1cb3
Eugene Skepner <github@skepner.eu>
2019-07-18 12:49:45 +0200

    in_json development

--- cc/in-json.hh
+++ cc/in-json.hh
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <stack>
+#include <memory>
+
 #include "acmacs-base/fmt.hh"
 
 // ----------------------------------------------------------------------
@@ -12,6 +15,13 @@ namespace in_json
         {
           public:
             template <typename S> error(size_t line_no, ssize_t column_no, S&& message) : std::runtime_error(fmt::format("in_json error at {}:{}: {}", line_no, column_no, message)) {}
+        };
+
+        class parse_error : public std::runtime_error
+        {
+          public:
+            parse_error(std::string_view m1) : std::runtime_error{fmt::format("in_json parsing error: {}", m1)} {}
+            parse_error(std::string_view m1, std::string_view m2) : parse_error(fmt::format("{}{}", m1, m2)) {}
         };
 
         namespace detail
@@ -72,6 +82,8 @@ namespace in_json
                 throw error(0, 0, "read_number internal");
             }
         } // namespace detail
+
+        // ----------------------------------------------------------------------
 
         template <typename Sink, typename Iter> void parse(Sink& sink, Iter first, Iter last)
         {
@@ -135,6 +147,63 @@ namespace in_json
                 ++first;
             }
         }
+
+        // ----------------------------------------------------------------------
+
+        class stack_entry
+        {
+          public:
+            stack_entry() = default;
+            stack_entry(const stack_entry&) = default;
+            virtual ~stack_entry() = default;
+            virtual const char* injson_name() = 0;
+            virtual std::unique_ptr<stack_entry> injson_put_object() { throw in_json::parse_error(fmt::format("{}: unexpected subobject for key \"{}\"", injson_name(), key_)); }
+            virtual void injson_put_key(std::string_view data) { key_ = data; }
+            virtual void injson_put_string(std::string_view data) { throw parse_error(fmt::format("{}: unexpected string \"{}\" for key \"{}\"", injson_name(), data, key_)); }
+            virtual void injson_put_integer(std::string_view data) { throw parse_error(fmt::format("{}: unexpected integer {} for key \"{}\"", injson_name(), data, key_)); }
+            virtual void injson_put_real(std::string_view data) { throw parse_error(fmt::format("{}: unexpected real {} for key \"{}\"", injson_name(), data, key_)); }
+            virtual void injson_put_array() { throw parse_error(fmt::format("{}: unexpected array for key \"{}\"", injson_name(), key_)); }
+            virtual void injson_pop_array() {} //  throw parse_error("stack_entry::injson_pop_array");
+            bool key_empty() const { return key_.empty(); }
+
+          protected:
+            std::string_view key_{};
+
+            void reset_key() { key_ = std::string_view{}; }
+        };
+
+        template <typename TargetContainer, typename ToplevelStackEntry> class object_sink
+        {
+          public:
+            object_sink(TargetContainer& target) : target_{target} {}
+
+            void injson_object_start()
+            {
+                if (stack_.empty())
+                    stack_.push(std::make_unique<ToplevelStackEntry>(target_));
+                else
+                    stack_.push(stack_.top()->injson_put_object());
+            }
+
+            void injson_object_end() { stack_.pop(); }
+            void injson_array_start() { stack_.top()->injson_put_array(); }
+            void injson_array_end() { stack_.top()->injson_pop_array(); }
+            template <typename Iter> void injson_string(Iter first, Iter last)
+            {
+                const std::string_view data{&*first, static_cast<size_t>(last - first)};
+                if (auto& tar = *stack_.top(); tar.key_empty())
+                    tar.injson_put_key(data);
+                else
+                    tar.injson_put_string(data);
+            }
+            template <typename Iter> void injson_integer(Iter first, Iter last) { stack_.top()->injson_put_integer({&*first, static_cast<size_t>(last - first)}); }
+            template <typename Iter> void injson_real(Iter first, Iter last) { stack_.top()->injson_put_real({&*first, static_cast<size_t>(last - first)}); }
+
+          private:
+            TargetContainer& target_;
+            std::stack<std::unique_ptr<stack_entry>> stack_;
+        };
+
     } // namespace v1
 } // namespace in_json
 
