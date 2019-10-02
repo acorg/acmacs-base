47300964da46f9383f742c7947877dc26a287495
Eugene Skepner <github@skepner.eu>
2019-10-02 17:07:05 +0200

    settings-v2

---
+++ test/test-settings.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+TDIR=$(mktemp -d -t XXXXXX)
+TESTDIR=$(dirname $0)
+
+# ======================================================================
+
+function on_exit
+{
+    rm -rf "$TDIR"
+}
+
+trap on_exit EXIT
+
+function failed
+{
+    echo ERROR: "$@" FAILED >&2
+    exit 1
+}
+
+trap failed ERR
+
+# ======================================================================
+
+cd "$TESTDIR"
+# ../dist/test-settings
+../dist/test-settings >"${TDIR}/test-settings.output.txt"
+if ! cmp "${TDIR}/test-settings.output.txt" test-settings-expected-output.txt; then
+    diff "${TDIR}/test-settings.output.txt" test-settings-expected-output.txt
+    false
+fi
