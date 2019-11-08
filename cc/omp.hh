6cb8a8822e81c554a319cd97a45765b1b20280ec
Eugene Skepner <github@skepner.eu>
2019-11-08 15:42:22 +0100

    porting to macOS 10.14.6

--- cc/omp.hh
+++ cc/omp.hh
@@ -3,14 +3,21 @@
 // ----------------------------------------------------------------------
 #ifdef _OPENMP
 
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wreserved-id-macro"
+#endif
+
 #include <omp.h>
+
+#pragma GCC diagnostic pop
 
 // ----------------------------------------------------------------------
 #else
 
-inline int omp_get_thread_num() { return 0; }
-inline int omp_get_num_threads() { return 1; }
-inline int omp_get_max_threads() { return 1; }
+constexpr inline int omp_get_thread_num() { return 0; }
+constexpr inline int omp_get_num_threads() { return 1; }
+constexpr inline int omp_get_max_threads() { return 1; }
 
 #endif
 
