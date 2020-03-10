94b659a31371ef973ce946e18810a083e56558cd
Eugene Skepner <github@skepner.eu>
2020-03-10 13:16:23 +0100

    map_with_duplicating_keys_t

--- cc/flat-map.hh
+++ cc/flat-map.hh
@@ -3,11 +3,82 @@
 #include <vector>
 
 #include "acmacs-base/fmt.hh"
+#include "acmacs-base/range-v3.hh"
 
 // ----------------------------------------------------------------------
 
 namespace acmacs
 {
+    // see seqdb.cc Seqdb::hash_index() for sample usage
+    template <typename Key, typename Value> class map_with_duplicating_keys_t
+    {
+      public:
+        using entry_type = std::pair<Key, Value>;
+        using const_iterator = typename std::vector<entry_type>::const_iterator;
+
+        map_with_duplicating_keys_t() = default;
+
+        template <typename Range> void collect(Range&& rng)
+        {
+            data_ = rng | ranges::to<std::vector>;
+            ranges::sort(data_, [](const auto& e1, const auto& e2) { return e1.first < e2.first; });
+        }
+
+        bool empty() const noexcept { return data_.empty(); }
+
+        template <typename FindKey> std::pair<const_iterator, const_iterator> find(const FindKey& key) const noexcept
+        {
+            const auto first = std::lower_bound(std::begin(data_), std::end(data_), key, [](const auto& e1, const auto& k2) { return e1.first < k2; });
+            // first may point to the wrong key, if key is not in the map
+            return {first, std::find_if(first, std::end(data_), [&key](const auto& en) { return en.first != key; })};
+        }
+
+        constexpr const auto& data() const { return data_; }
+
+      private:
+        std::vector<entry_type> data_;
+    };
+
+    // ----------------------------------------------------------------------
+
+    // see seqdb.cc Seqdb::seq_id_index() for sample usage
+    template <typename Key, typename Value> class map_with_unique_keys_t
+    {
+      public:
+        using entry_type = std::pair<Key, Value>;
+
+        map_with_unique_keys_t() = default;
+
+        template <typename Range> void collect(Range&& rng, bool check = true)
+        {
+            data_ = rng | ranges::to<std::vector>;
+            ranges::sort(data_, [](const auto& e1, const auto& e2) { return e1.first < e2.first; });
+            if (check && data_.size() > 1) {
+                for (auto cur = std::next(std::begin(data_)); cur != std::end(data_); ++cur) {
+                    if (cur->first == std::prev(cur)->first)
+                        throw std::runtime_error{"duplicating keys within map_with_unique_keys_t"};
+                }
+            }
+        }
+
+        bool empty() const noexcept { return data_.empty(); }
+
+        const Value* find(const Key& key) const noexcept
+        {
+            if (const auto first = std::lower_bound(std::begin(data_), std::end(data_), key, [](const auto& e1, const auto& k2) { return e1.first < k2; }); first->first == key)
+                return &first->second;
+            else
+                return nullptr;
+        }
+
+        constexpr const auto& data() const { return data_; }
+
+      private:
+        std::vector<entry_type> data_;
+    };
+
+    // ----------------------------------------------------------------------
+
     template <typename Key, typename Value> class flat_map_t
     {
       public:
@@ -24,12 +95,30 @@ namespace acmacs
         auto empty() const { return data_.empty(); }
         auto size() const { return data_.size(); }
         const auto& operator[](size_t index) const { return data_[index]; }
-        void sort_by_key() { std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.first < e2.first; }); }
-        void sort_by_value() { std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.second < e2.second; }); }
-        void sort_by_value_reverse() { std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.second > e2.second; }); }
+        const auto& back() const { return data_.back(); }
+        auto& back() { return data_.back(); }
+        void sort_by_key()
+        {
+            std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.first < e2.first; });
+        }
+        void sort_by_value()
+        {
+            std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.second < e2.second; });
+        }
+        void sort_by_value_reverse()
+        {
+            std::sort(std::begin(data_), std::end(data_), [](const auto& e1, const auto& e2) { return e1.second > e2.second; });
+        }
 
-        template <typename K> auto find(const K& key) const { return std::find_if(std::begin(data_), std::end(data_), [&key](const auto& en) { return en.first == key; }); }
-        template <typename K> auto find(const K& key) { return std::find_if(std::begin(data_), std::end(data_), [&key](const auto& en) { return en.first == key; }); }
+        template <typename K> auto find(const K& key) const
+        {
+            return std::find_if(std::begin(data_), std::end(data_), [&key](const auto& en) { return en.first == key; });
+        }
+
+        template <typename K> auto find(const K& key)
+        {
+            return std::find_if(std::begin(data_), std::end(data_), [&key](const auto& en) { return en.first == key; });
+        }
         template <typename K> bool exists(const K& key) const { return find(key) != std::end(data_); }
 
         Value& at(const Key& key)
