dcdd2686aebb655a7388ce68e172e6f12633b808
Eugene Skepner <github@skepner.eu>
2020-03-13 16:11:50 +0100

    LOG

--- cc/debug.hh
+++ cc/debug.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include "acmacs-base/fmt.hh"
 
 // ----------------------------------------------------------------------
@@ -8,6 +9,9 @@
 #define DEBUG_FILE_LINE_FUNC_S fmt::format("{}:{}: {}", __FILE__, __LINE__, __PRETTY_FUNCTION__)
 #define DEBUG_FILE_LINE_FUNC fmt::format(" [{}]", DEBUG_FILE_LINE_FUNC_S)
 
+#define LOG(section, ...) acmacs::log::message(section, [&]() { return fmt::format("{} [{}]", fmt::format(__VA_ARGS__), DEBUG_FILE_LINE); })
+#define LOGF(section, ...) acmacs::log::message(section, [&]() { return fmt::format("{}{}", fmt::format(__VA_ARGS__), DEBUG_FILE_LINE_FUNC); })
+
 // ----------------------------------------------------------------------
 
 namespace acmacs
@@ -15,8 +19,21 @@ namespace acmacs
     enum class debug { no, yes };
     enum class verbose { no, yes };
 
+    template <typename Int> constexpr auto number_of_decimal_digits(Int max_value) { return static_cast<int>(std::log10(max_value)) + 1; }
+
     constexpr inline debug debug_from(bool verb) { return verb ? debug::yes : debug::no; }
     constexpr inline verbose verbose_from(bool verb) { return verb ? verbose::yes : verbose::no; }
+
+    namespace log::inline v1
+    {
+        using section_t = uint32_t;
+
+        template <typename MesssageGetter> void message(section_t section, MesssageGetter get_message)
+        {
+            if (section)
+                fmt::print(stderr, "{}\n", get_message());
+        }
+    }
 
     // class debug
     // {
