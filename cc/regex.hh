cba7b59a9be0b9d750da544be0f6b0c150e56921
Eugene Skepner <github@skepner.eu>
2020-04-05 11:39:50 +0200

    helper functions to scan over mutiple regex

--- cc/regex.hh
+++ cc/regex.hh
@@ -4,7 +4,52 @@
 
 #include "acmacs-base/fmt.hh"
 
-// ----------------------------------------------------------------------
+// ======================================================================
+// Support for multiple regex and replacements, see acmacs-virus/cc/reassortant.cc
+// ======================================================================
+
+namespace acmacs::regex
+{
+    struct look_replace_t
+    {
+        const std::regex look_for;
+        const char* replace_fmt; // syntax: http://ecma-international.org/ecma-262/5.1/#sec-15.5.4.11
+    };
+
+    // returns empty string if no matches found
+    template <typename Container> std::string scan_replace(std::string_view source, const Container& scan_data)
+    {
+        for (const auto& entry : scan_data) {
+            if (std::cmatch match; std::regex_search(std::begin(source), std::end(source), match, entry.look_for))
+                return match.format(entry.replace_fmt);
+        }
+        return {};
+    }
+
+    // ----------------------------------------------------------------------
+
+    struct look_replace2_t
+    {
+        const std::regex look_for;
+        const char* replace_fmt1;
+        const char* replace_fmt2;
+    };
+
+    // returns pair of empty strings if no matches found
+    template <typename Container> std::pair<std::string, std::string> scan_replace2(std::string_view source, const Container& scan_data)
+    {
+        for (const auto& entry : scan_data) {
+            if (std::cmatch match; std::regex_search(std::begin(source), std::end(source), match, entry.look_for))
+                return {match.format(entry.replace_fmt1), match.format(entry.replace_fmt2)};
+        }
+        return {};
+    }
+
+}
+
+// ======================================================================
+// fmt support for std::smatch and std::cmatch
+// ======================================================================
 
 // specialization below follows fmt lib description, but it does not work due to ambiguity with
 // template <typename RangeT, typename Char> struct formatter<RangeT, Char, enable_if_t<fmt::is_range<RangeT, Char>::value>>
