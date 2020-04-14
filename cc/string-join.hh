f95546bb7eacd61e5f49a50814c2fe94e62c0452
Eugene Skepner <github@skepner.eu>
2020-04-14 12:54:09 +0200

    string::join improvements

--- cc/string-join.hh
+++ cc/string-join.hh
@@ -7,47 +7,34 @@
 
 namespace acmacs::string
 {
+    namespace detail::join
+    {
+        constexpr inline std::string_view stringify(std::string_view source) { return source; }
+        constexpr inline std::string_view stringify(const char* source) { return source; }
+        constexpr inline const std::string& stringify(const std::string& source) { return source; }
+
+        template <typename Arg> inline std::enable_if_t<!acmacs::sfinae::is_string_v<Arg>, std::string> stringify(const Arg& arg) { return fmt::format("{}", arg); }
+    } // namespace detail::join
+
     template <typename Arg1, typename Arg2, typename... Args>
-    std::enable_if_t<!acmacs::sfinae::is_iterator_v<Arg1> || acmacs::sfinae::is_string_v<Arg1>, std::string> join(std::string_view separator, Arg1&& arg1, Arg2&& arg2, Args&&... rest)
+    inline std::enable_if_t<!acmacs::sfinae::is_iterator_v<Arg1> || acmacs::sfinae::is_string_v<Arg1>, std::string> join(std::string_view separator, Arg1&& arg1, Arg2&& arg2, Args&&... rest)
     {
         if constexpr (sizeof...(rest) == 0) {
-            if constexpr (acmacs::sfinae::is_string_v<Arg1>) {
-                const std::string_view asv1{arg1};
-                if constexpr (acmacs::sfinae::is_string_v<Arg2>) {
-                    if (const std::string_view asv2{arg2}; !asv1.empty()) {
-                        if (!asv2.empty()) {
-                            std::string res(asv1.size() + asv2.size() + separator.size(), '#');
-                            // AD_DEBUG("join strings 1:{} 2:{} sep:{} -> \"{}\"", asv1.size(), asv2.size(), separator.size(), res);
-                            std::copy(asv1.begin(), asv1.end(), res.begin());
-                            // AD_DEBUG("  res1: \"{}\"", res);
-                            std::copy(separator.begin(), separator.end(), std::next(res.begin(), static_cast<ssize_t>(asv1.size())));
-                            // AD_DEBUG("  res2: \"{}\"", res);
-                            std::copy(asv2.begin(), asv2.end(), std::next(res.begin(), static_cast<ssize_t>(asv1.size() + separator.size())));
-                            // AD_DEBUG("  res3: \"{}\"", res);
-                            return res;
-                        }
-                        else
-                            return std::string{asv1};
-                    }
-                    else
-                        return std::string{asv2};
+            const auto as1{detail::join::stringify(arg1)};
+            const auto as2{detail::join::stringify(arg2)};
+            if (!as1.empty()) {
+                if (!as2.empty()) {
+                    std::string res(as1.size() + as2.size() + separator.size(), '#');
+                    std::copy(as1.begin(), as1.end(), res.begin());
+                    std::copy(separator.begin(), separator.end(), std::next(res.begin(), static_cast<ssize_t>(as1.size())));
+                    std::copy(as2.begin(), as2.end(), std::next(res.begin(), static_cast<ssize_t>(as1.size() + separator.size())));
+                    return res;
                 }
-                else {
-                    if (!asv1.empty())
-                        return fmt::format("{}{}{}", asv1, separator, arg2);
-                    else
-                        return fmt::format("{}", arg2);
-                }
-            }
-            else if constexpr (acmacs::sfinae::is_string_v<Arg2>) {
-                if (const std::string_view asv2{arg2}; !asv2.empty())
-                    return fmt::format("{}{}{}", arg1, separator, asv2);
                 else
-                    return fmt::format("{}", arg1);
+                    return std::string{as1};
             }
-            else {
-                return fmt::format("{}{}{}", arg1, separator, arg2);
-            }
+            else
+                return std::string{as2};
         }
         else {
             return join(separator, join(separator, std::forward<Arg1>(arg1), std::forward<Arg2>(arg2)), std::forward<Args>(rest)...);
@@ -106,16 +93,9 @@ namespace acmacs::string
 
     template <typename Collection> inline std::string join(std::string_view separator, const Collection& values) { return join(separator, std::begin(values), std::end(values)); }
 
-    inline std::string join(std::string_view separator, std::initializer_list<std::string_view>&& values) { return join(separator, std::begin(values), std::end(values)); }
-
-    inline std::string join(std::initializer_list<std::string_view>&& parts) { return join(" ", std::begin(parts), std::end(parts)); }
-
     // ----------------------------------------------------------------------
 
-    template <typename ... Args> inline std::string concat(Args&& ... args)
-    {
-        return join("", std::forward<Args>(args) ...);
-    }
+    template <typename... Args> inline std::string concat(Args&&... args) { return join("", std::forward<Args>(args)...); }
 
 } // namespace acmacs::string
 
