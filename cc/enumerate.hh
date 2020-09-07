f34940d8eac609b2e83e92ab184db93f250a5171
Eugene Skepner <github@skepner.eu>
2020-09-07 11:42:05 +0200

    acmacs::enumerate improvement

--- cc/enumerate.hh
+++ cc/enumerate.hh
@@ -53,24 +53,24 @@ namespace acmacs
         return _enumerate_internal::enumerate_range<Iterator, index_type>(first, last, initial);
     }
 
-    template <typename Container, typename index_type = size_t> inline auto enumerate(Container& content, size_t initial = 0)
+    template <typename Container, typename index_type = size_t, typename = std::enable_if_t<std::is_integral_v<index_type>>> inline auto enumerate(Container& content, index_type initial = 0)
     {
         using iter_type = decltype(std::begin(content));
         return _enumerate_internal::enumerate_range<iter_type, index_type>(std::begin(content), std::end(content), initial);
     }
 
-    template <typename Container, typename index_type = size_t> inline auto enumerate(const Container& content, size_t initial = 0)
+    template <typename Container, typename index_type = size_t, typename = std::enable_if_t<std::is_integral_v<index_type>>> inline auto enumerate(const Container& content, index_type initial = 0)
     {
         using iter_type = decltype(std::begin(content));
         return _enumerate_internal::enumerate_range<iter_type, index_type>(std::begin(content), std::end(content), initial);
     }
 
-    template <typename Container, typename index_type = size_t> inline auto enumerate(Container&&, size_t = 0)
+    template <typename Container, typename index_type = size_t, typename = std::enable_if_t<std::is_integral_v<index_type>>> inline auto enumerate(Container&&, index_type = 0)
     {
         static_assert(std::is_same_v<int, Container>, "acmacs::enumerate cannot use temp (&&) values as a container (g++9 will destroy container before starting enumeration");
     }
 
-    template <typename Container, typename Func, typename Index = size_t> inline void enumerate(Container&& content, Func callback, Index index = 0)
+    template <typename Container, typename Func, typename index_type = size_t, typename = std::enable_if_t<std::is_integral_v<index_type>>> inline void enumerate(Container&& content, Func callback, index_type index = 0)
     {
         for (auto& element : content) {
             callback(index, element);
