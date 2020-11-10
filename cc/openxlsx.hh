2e5b4141e00bd8ef52006a74cb224e4f1c26efed
Eugene Skepner <github@skepner.eu>
2020-11-10 17:33:56 +0100

    OpenXLSX support

---
+++ cc/openxlsx.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <optional>
+
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wnewline-eof"
+#pragma GCC diagnostic ignored "-Wextra-semi"
+#endif
+
+#ifdef __GNUG__
+#endif
+
+#include <OpenXLSX/OpenXLSX.hpp>
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
