2769413e4692843498af90d589f6658854575a0c
Eugene Skepner <github@skepner.eu>
2020-11-11 10:05:45 +0100

    OpenXLSX support

--- cc/openxlsx.hh
+++ cc/openxlsx.hh
@@ -3,10 +3,12 @@
 #include <optional>
 
 #pragma GCC diagnostic push
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
 #ifdef __clang__
 #pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
 #pragma GCC diagnostic ignored "-Wdocumentation"
-#pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #pragma GCC diagnostic ignored "-Wnewline-eof"
 #pragma GCC diagnostic ignored "-Wextra-semi"
 #endif
