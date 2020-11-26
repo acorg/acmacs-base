897a900945a1cafa75e2f2fc7f0ad2f6d0bfad35
Eugene Skepner <github@skepner.eu>
2020-11-26 15:38:37 +0100

    chaiscript support

---
+++ cc/chaiscript.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdocumentation"
+#pragma GCC diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma GCC diagnostic ignored "-Wexit-time-destructors"
+#pragma GCC diagnostic ignored "-Wdefaulted-function-deleted"
+#pragma GCC diagnostic ignored "-Wrange-loop-construct"
+#pragma GCC diagnostic ignored "-Wswitch-enum"
+#pragma GCC diagnostic ignored "-Wdeprecated-this-capture"
+#pragma GCC diagnostic ignored "-Wtautological-type-limit-compare"
+#pragma GCC diagnostic ignored "-Wtautological-unsigned-zero-compare"
+#pragma GCC diagnostic ignored "-Wshadow-field"
+// #pragma GCC diagnostic ignored ""
+#endif
+
+#ifdef __GNUG__
+#endif
+
+#include <chaiscript/chaiscript.hpp>
+
+#pragma GCC diagnostic pop
+
+
+// ----------------------------------------------------------------------
+/// Local Variables:
+/// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+/// End:
