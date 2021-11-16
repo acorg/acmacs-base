2d2830df942cbee2cbc244dab2b5366665b40cca
Eugene Skepner <github@skepner.eu>
2021-11-16 15:01:21 +0100

    acmacs-data support

--- cc/acmacsd.hh
+++ cc/acmacsd.hh
@@ -18,6 +18,30 @@ namespace acmacs
         throw std::runtime_error("ACMACSD_ROOT env is not set (for apache-mod-acmacs see /etc/apache2/envvars)");
     }
 
+    inline std::string hidb_v5_dir()
+    {
+        using namespace std::string_literals;
+        if (auto dir = std::getenv("HIDB_V5"); dir)
+            return dir;
+        throw std::runtime_error("HIDB_V5 env is not set (for apache-mod-acmacs see /etc/apache2/envvars)");
+    }
+
+    inline std::string locdb_v2()
+    {
+        using namespace std::string_literals;
+        if (auto dir = std::getenv("LOCATIONDB_V2"); dir)
+            return dir;
+        throw std::runtime_error("LOCATIONDB_V2 env is not set (for apache-mod-acmacs see /etc/apache2/envvars)");
+    }
+
+    inline std::string seqdb_v3_dir()
+    {
+        using namespace std::string_literals;
+        if (auto dir = std::getenv("SEQDB_V3"); dir)
+            return dir;
+        throw std::runtime_error("SEQDB_V3 env is not set (for apache-mod-acmacs see /etc/apache2/envvars)");
+    }
+
 } // namespace acmacs
 
 // ----------------------------------------------------------------------
